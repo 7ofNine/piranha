cd940a2e6eea288b0d77a8edee05ca55181e95e9
bluescarni <bluescarni@f32a13d4-301a-0410-ba96-7b22a51b2342>
2007-06-07 16:16:47 +0000

    Use 120dpi as default resolution for latex rendering.
    git-svn-id: https://piranha-psm.googlecode.com/svn/trunk@60 f32a13d4-301a-0410-ba96-7b22a51b2342

--- pyranha/Gui/__panelWidget.py
+++ pyranha/Gui/__panelWidget.py
@@ -47,6 +47,9 @@ class panelWidget(QtGui.QWidget):
     self.connect(self.ui.fpComboBox,QtCore.SIGNAL("currentIndexChanged(int)"),self.__setFpRep)
     self.connect(self.ui.latexRenderCheckBox,QtCore.SIGNAL("stateChanged(int)"),self.__changeLatexRender)
     self.show()
+  #class __latexRenderThread(QtCore.QThread):
+   # def run(self):
+    #  
   def __setTheoriesPathDialog(self):
     dialog=QtGui.QFileDialog()
     dialog.setFileMode(QtGui.QFileDialog.DirectoryOnly)
@@ -123,7 +126,7 @@ class panelWidget(QtGui.QWidget):
     dvipngProcess=QtCore.QProcess()
     dvipngProcess.setWorkingDirectory(QtCore.QDir.tempPath())
     arguments.clear()
-    arguments << "--dvinum*" << "-T" << "tight" << "qt_temp.dvi" << "-bg" << "Transparent" << "-o" <<  tmpFilePng.fileName()
+    arguments << "--dvinum*" << "-T" << "tight" << "-D" << "120" << "-bg" << "Transparent" << "qt_temp.dvi" << "-o" <<  tmpFilePng.fileName()
     dvipngProcess.start("dvipng",arguments)
     dvipngProcess.waitForFinished()
     dvipngErr=QtCore.QString(dvipngProcess.readAllStandardOutput())
