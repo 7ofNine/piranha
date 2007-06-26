061b562c943d80f00c05ce2450f4d88f7d28d621
bluescarni <bluescarni@f32a13d4-301a-0410-ba96-7b22a51b2342>
2007-06-26 10:22:29 +0000

    Disable for now numpy and matplotlib import in python.
    We need a way to make this really optional.
    
    
    git-svn-id: https://piranha-psm.googlecode.com/svn/trunk@126 f32a13d4-301a-0410-ba96-7b22a51b2342

--- pyranha/__init__.py
+++ pyranha/__init__.py
@@ -16,8 +16,8 @@
 # Free Software Foundation, Inc.,
 # 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-from numpy import *
-from matplotlib.pylab import *
+#from numpy import *
+#from matplotlib.pylab import *
 from copy import *
 
 # Global variables: theories of motion
