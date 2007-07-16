0ba4a04b6a06f17e1684d2e9169159a56b3227cb
bluescarni <bluescarni@f32a13d4-301a-0410-ba96-7b22a51b2342>
2007-07-16 10:13:37 +0000

    Minor enhancement to beautifier tool.
    git-svn-id: https://piranha-psm.googlecode.com/svn/trunk@168 f32a13d4-301a-0410-ba96-7b22a51b2342

--- tools/beautify_bcpp.sh
+++ tools/beautify_bcpp.sh
@@ -1,6 +1,13 @@
 #! /bin/bash
 
-for i in `find ./ -regex "\(.*\.h\\|.*\.cpp\)"`
-  do bcpp -i 2 "${i}" > "${i}".new
-  mv "${i}".new "${i}"
-done
+if [[ "$@" == "" ]]
+then for i in `find ./ -regex "\(.*\.h\\|.*\.cpp\)"`
+    do bcpp -i 2 "${i}" > "${i}".new
+    mv "${i}".new "${i}"
+  done
+else
+  for i in "$@"
+    do bcpp -i 2 "${i}" > "${i}".new
+    mv "${i}".new "${i}"
+  done
+fi
