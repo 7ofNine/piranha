7ofNine/piranha

commit f9df85dac7e7237c82576ce6f8336919dfbbbe86

Use correctly C++'s getline function.
git-svn-id: https://piranha-psm.googlecode.com/svn/trunk@210 f32a13d4-301a-0410-ba96-7b22a51b2342

src/utils.h
@@ -65,11 +65,9 @@ namespace piranha
  */
       static int get_valid_string(std::ifstream &inf, std::string &str)
       {
-        bool theres_more;
         do
         {
-          theres_more=getline(inf,str,'\n');
-          if (theres_more==false)
+          if (getline(inf,str,'\n').eof())
           {
             return 1;
           }

commit fddd71c226d4194df37e9e5387cc95b8873f3020

Reworked correct getline usage to fix a bug introduced in f9df85dac7e7237c82576ce6f8336919dfbbbe86
We were discarding the last term of loaded series, since we checked for eof
_after_ the last line read.

Checked with tass_r6, 9883 terms.


git-svn-id: https://piranha-psm.googlecode.com/svn/trunk@240 f32a13d4-301a-0410-ba96-7b22a51b2342

src/utils.h
@@ -65,14 +65,13 @@ namespace piranha
  */
       static int get_valid_string(std::ifstream &inf, std::string &str)
       {
-        bool theres_more;
         do
         {
-          theres_more=getline(inf,str,'\n');
-          if (theres_more==false)
+          if (inf.eof())
           {
             return 1;
           }
+          getline(inf,str,'\n');
           boost::trim(str);
         }
         while (is_invalid_string(str));
@@ -144,7 +143,7 @@ namespace piranha
 /// Check whether a string is empty or commented.
       static bool is_invalid_string(std::string &str)
       {
-        if (str=="")
+        if (str.empty())
         {
           return true;
         }