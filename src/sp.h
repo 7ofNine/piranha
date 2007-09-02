e5acc2ce2910710e6eea246b406422851b94ab8f
bluescarni <bluescarni@f32a13d4-301a-0410-ba96-7b22a51b2342>
2007-09-02 21:38:00 +0000

    Simplify sps's indices.
    git-svn-id: https://piranha-psm.googlecode.com/svn/trunk@456 f32a13d4-301a-0410-ba96-7b22a51b2342

--- src/sp.h
+++ src/sp.h
@@ -77,20 +77,13 @@ namespace piranha
       Term<Cf, Trig>,
       min_expo_extractor<Term<Cf, Trig> >,
       degree_extractor<Term<Cf, Trig> >,
-      boost::multi_index::const_mem_fun < Term<Cf, Trig>, const bool &,
-      &Term<Cf, Trig>::g_flavour > ,
       boost::multi_index::const_mem_fun < Term<Cf, Trig>, const Trig &,
       &Term<Cf, Trig>::g_trig_ref >
       >
       >,
       boost::multi_index::hashed_unique <
-      boost::multi_index::composite_key <
-      Term<Cf, Trig>,
-      boost::multi_index::const_mem_fun < Term<Cf, Trig>, const bool &,
-      &Term<Cf, Trig>::g_flavour > ,
       boost::multi_index::const_mem_fun < Term<Cf, Trig>, const Trig &,
       &Term<Cf, Trig>::g_trig_ref >
-      >
       >
       > type;
   };
