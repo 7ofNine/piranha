0d3161f43b3767386915f007f91eb84e55cf0914
bluescarni <bluescarni@f32a13d4-301a-0410-ba96-7b22a51b2342>
2007-09-02 21:37:45 +0000

    Fix sc to work with simplified indices.
    git-svn-id: https://piranha-psm.googlecode.com/svn/trunk@454 f32a13d4-301a-0410-ba96-7b22a51b2342

--- src/prectest.h
+++ src/prectest.h
@@ -91,8 +91,7 @@ namespace piranha
     double max_diff=0.;
     for (typename T::it_s_index it=ps1.g_s_index().begin();it!=it_f;++it)
     {
-      it_h=ps2.g_h_index().find(boost::make_tuple(it->g_flavour(),
-        *it->g_trig()));
+      it_h=ps2.g_h_index().find(*it->g_trig());
       if (it_h==ps2.g_h_index().end())
       {
         diffs_[i]=-.1;
