7ffb034ee53015eba40a0ab874ab88d6fca40130
bluescarni <bluescarni@f32a13d4-301a-0410-ba96-7b22a51b2342>
2007-09-26 10:31:13 +0000

    Added global piranha header, useful for main.cpp files.
    git-svn-id: https://piranha-psm.googlecode.com/svn/trunk@534 f32a13d4-301a-0410-ba96-7b22a51b2342

---
+++ src/piranha.h
@@ -0,0 +1,86 @@
+/***************************************************************************
+ *   Copyright (C) 2007 by Francesco Biscani   *
+ *   bluescarni@gmail.com   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#ifndef PIRANHA_PIRANHA_H
+#define PIRANHA_PIRANHA_H
+
+// Include all piranha headers.
+
+#include "arg_manager.h"
+#include "astro.h"
+#include "base_ipoly.h"
+#include "base_polynomial.h"
+#include "base_pseries_def.h"
+#include "base_pseries.h"
+#include "base_pseries_hooks.h"
+#include "base_pseries_io.h"
+#include "base_pseries_manip.h"
+#include "base_pseries_math.h"
+#include "base_pseries_probe.h"
+#include "base_term.h"
+#include "common_typedefs.h"
+#include "complex_toolbox.h"
+#include "differential_toolbox.h"
+#include "double_cf.h"
+#include "fnp.h"
+#include "imonomial.h"
+#include "integral_npow_cache.h"
+#include "ipoly.h"
+// Disabled for now.
+//#include "keplerian_processor.h"
+#include "lnpc.h"
+#include "lnp.h"
+#include "math.h"
+#include "math_toolbox.h"
+#include "monomial_gmp_array.h"
+#include "norm_based_elementary_math_toolbox.h"
+#include "norm_index.h"
+#include "npc.h"
+#include "np.h"
+#include "operators_toolbox.h"
+#include "p_assert.h"
+#include "phase_list.h"
+#include "piranha_tbb.h"
+#include "platform_switches.h"
+#include "polynomial.h"
+#include "prectest.h"
+#include "ps.h"
+#include "psymbol.h"
+#include "settings_manager.h"
+#include "simple_container.h"
+#include "simple_term.h"
+// Disabled for now.
+//#include "spc.h"
+#include "sp.h"
+#include "stats.h"
+#include "stream_manager.h"
+#include "symbol_limiter.h"
+#include "symbol_limiting_elementary_math_toolbox.h"
+#include "tass17/tass17.h"
+#include "trig_array.h"
+#include "trig_cow_term.h"
+#include "trig_evaluator.h"
+#include "trig_fixed_array.h"
+#include "trigonometric_toolbox.h"
+#include "trig_sparse_array.h"
+#include "utils.h"
+#include "version.h"
+
+#endif
