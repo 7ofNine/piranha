a9b240445805f29d4a8cf75118277f3d9c50205f
bluescarni <bluescarni@f32a13d4-301a-0410-ba96-7b22a51b2342>
2007-09-27 00:29:06 +0000

    Minor doc fix.
    git-svn-id: https://piranha-psm.googlecode.com/svn/trunk@540 f32a13d4-301a-0410-ba96-7b22a51b2342

--- src/simple_term.h
+++ src/simple_term.h
@@ -48,7 +48,7 @@ namespace piranha
       explicit simple_term(const cf_type &c):
       private_cf_(c),private_trig_()
         {}
-/// constructor from coefficient and trigonometric part.
+/// Constructor from coefficient and trigonometric part.
       explicit simple_term(const cf_type &c, const trig_type &t):
         private_cf_(c),private_trig_(t)
       {}
