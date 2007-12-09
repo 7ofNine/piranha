d8795767fa354a321ae0d83a0c7fe25d5c8f429d
bluescarni <bluescarni@gmail.com>
2007-12-09 17:01:18 +0100

    Move base operators into own macro.

--- src/bits/toolboxes/operators_toolbox.h
+++ src/bits/toolboxes/operators_toolbox.h
@@ -23,114 +23,126 @@
 
 namespace piranha
 {
+#define __PIRANHA_BASE_OPERATORS Derived &operator=(const Derived &p)\
+{\
+  return static_cast<Derived *>(this)->assign(p);\
+}\
+Derived &operator+=(int n)\
+{\
+  return static_cast<Derived *>(this)->add(n);\
+}\
+Derived &operator+=(const double &x)\
+{\
+  return static_cast<Derived *>(this)->add(x);\
+}\
+Derived &operator+=(const Derived &p)\
+{\
+  return static_cast<Derived *>(this)->add(p);\
+}\
+Derived operator+(int n) const\
+{\
+  Derived retval(*static_cast<Derived const *>(this));\
+  retval+=n;\
+  return retval;\
+}\
+Derived operator+(const double &x) const\
+{\
+  Derived retval(*static_cast<Derived const *>(this));\
+  retval+=x;\
+  return retval;\
+}\
+Derived operator+(const Derived &p) const\
+{\
+  Derived retval(*static_cast<Derived const *>(this));\
+  retval+=p;\
+  return retval;\
+}\
+Derived &operator-=(int n)\
+{\
+  return static_cast<Derived *>(this)->subtract(n);\
+}\
+Derived &operator-=(const double &x)\
+{\
+  return static_cast<Derived *>(this)->subtract(x);\
+}\
+Derived &operator-=(const Derived &p)\
+{\
+  return static_cast<Derived *>(this)->subtract(p);\
+}\
+Derived operator-(int n) const\
+{\
+  Derived retval(*static_cast<Derived const *>(this));\
+  retval-=n;\
+  return retval;\
+}\
+Derived operator-(const double &x) const\
+{\
+  Derived retval(*static_cast<Derived const *>(this));\
+  retval-=x;\
+  return retval;\
+}\
+Derived operator-(const Derived &p) const\
+{\
+  Derived retval(*static_cast<Derived const *>(this));\
+  retval-=p;\
+  return retval;\
+}\
+Derived &operator*=(int n)\
+{\
+  return static_cast<Derived *>(this)->mult_by(n);\
+}\
+Derived &operator*=(const double &x)\
+{\
+  return static_cast<Derived *>(this)->mult_by(x);\
+}\
+Derived &operator*=(const Derived &p)\
+{\
+  return static_cast<Derived *>(this)->mult_by(p);\
+}\
+Derived operator*(int n) const\
+{\
+  Derived retval(*static_cast<Derived const *>(this));\
+  retval*=n;\
+  return retval;\
+}\
+Derived operator*(const double &x) const\
+{\
+  Derived retval(*static_cast<Derived const *>(this));\
+  retval*=x;\
+  return retval;\
+}\
+Derived operator*(const Derived &p) const\
+{\
+  Derived retval(*static_cast<Derived const *>(this));\
+  retval*=p;\
+  return retval;\
+}\
+Derived &operator/=(int n)\
+{\
+  return static_cast<Derived *>(this)->divide_by(n);\
+}\
+Derived &operator/=(const double &x)\
+{\
+  return static_cast<Derived *>(this)->divide_by(x);\
+}\
+Derived operator/(int n) const\
+{\
+  Derived retval(*static_cast<Derived const *>(this));\
+  retval/=n;\
+  return retval;\
+}\
+Derived operator/(const double &x) const\
+{\
+  Derived retval(*static_cast<Derived const *>(this));\
+  retval/=x;\
+  return retval;\
+}
+
   template <class Derived>
     class real_operators_toolbox
   {
     public:
-      Derived &operator=(const Derived &p)
-      {
-        return static_cast<Derived *>(this)->assign(p);
-      }
-      Derived &operator+=(int n)
-      {
-        return static_cast<Derived *>(this)->add(n);
-      }
-      Derived &operator+=(const double &x)
-      {
-        return static_cast<Derived *>(this)->add(x);
-      }
-      Derived &operator+=(const Derived &p)
-      {
-        return static_cast<Derived *>(this)->add(p);
-      }
-      Derived operator+(int n) const
-      {
-        Derived retval(*static_cast<Derived const *>(this));
-        retval+=n;
-        return retval;
-      }
-      Derived operator+(const double &x) const
-      {
-        Derived retval(*static_cast<Derived const *>(this));
-        retval+=x;
-        return retval;
-      }
-      Derived operator+(const Derived &p) const
-      {
-        Derived retval(*static_cast<Derived const *>(this));
-        retval+=p;
-        return retval;
-      }
-      Derived &operator-=(const double &x)
-      {
-        return static_cast<Derived *>(this)->add_generic(-x);
-      }
-      Derived &operator-=(const Derived &p)
-      {
-        return static_cast<Derived *>(this)->subtract(p);
-      }
-      Derived operator-(const double &x) const
-      {
-        Derived retval(*static_cast<Derived const *>(this));
-        retval-=x;
-        return retval;
-      }
-      Derived operator-(const Derived &p) const
-      {
-        Derived retval(*static_cast<Derived const *>(this));
-        retval-=p;
-        return retval;
-      }
-      Derived &operator*=(int n)
-      {
-        return static_cast<Derived *>(this)->mult_by(n);
-      }
-      Derived &operator*=(const double &x)
-      {
-        return static_cast<Derived *>(this)->mult_by(x);
-      }
-      Derived &operator*=(const Derived &p)
-      {
-        return static_cast<Derived *>(this)->mult_by(p);
-      }
-      Derived operator*(int n) const
-      {
-        Derived retval(*static_cast<Derived const *>(this));
-        retval*=n;
-        return retval;
-      }
-      Derived operator*(const double &x) const
-      {
-        Derived retval(*static_cast<Derived const *>(this));
-        retval*=x;
-        return retval;
-      }
-      Derived operator*(const Derived &p) const
-      {
-        Derived retval(*static_cast<Derived const *>(this));
-        retval*=p;
-        return retval;
-      }
-      Derived &operator/=(int n)
-      {
-        return static_cast<Derived *>(this)->divide_by(n);
-      }
-      Derived &operator/=(const double &x)
-      {
-        return static_cast<Derived *>(this)->divide_by(x);
-      }
-      Derived operator/(const double &x) const
-      {
-        Derived retval(*static_cast<Derived const *>(this));
-        retval/=x;
-        return retval;
-      }
-      Derived operator/(int n) const
-      {
-        Derived retval(*static_cast<Derived const *>(this));
-        retval/=n;
-        return retval;
-      }
+      __PIRANHA_BASE_OPERATORS
   };
 
 // Requires: complex toolbox.
@@ -139,104 +151,7 @@ namespace piranha
   {
       typedef std::complex<real_Derived> Derived;
     public:
-      Derived &operator=(const Derived &p)
-      {
-        return static_cast<Derived *>(this)->assign(p);
-      }
-      Derived &operator+=(const double &x)
-      {
-        return static_cast<Derived *>(this)->add_generic(x);
-      }
-      Derived &operator+=(const Derived &p)
-      {
-        return static_cast<Derived *>(this)->add(p);
-      }
-      Derived operator+(const double &x) const
-      {
-        Derived retval(*static_cast<Derived const *>(this));
-        retval+=x;
-        return retval;
-      }
-      Derived operator+(const Derived &p) const
-      {
-        Derived retval(*static_cast<Derived const *>(this));
-        retval+=p;
-        return retval;
-      }
-      Derived &operator-=(const double &x)
-      {
-        return static_cast<Derived *>(this)->add_generic(-x);
-      }
-      Derived &operator-=(const Derived &p)
-      {
-        return static_cast<Derived *>(this)->subtract(p);
-      }
-      Derived operator-(const double &x) const
-      {
-        Derived retval(*static_cast<Derived const *>(this));
-        retval-=x;
-        return retval;
-      }
-      Derived operator-(const Derived &p) const
-      {
-        Derived retval(*static_cast<Derived const *>(this));
-        retval-=p;
-        return retval;
-      }
-      Derived &operator*=(int n)
-      {
-        static_cast<Derived *>(this)->mult_by_int(n);
-        return *static_cast<Derived *>(this);
-      }
-      Derived &operator*=(const double &x)
-      {
-// TODO: return in just one line.
-        static_cast<Derived *>(this)->mult_by_double(x);
-        return *static_cast<Derived *>(this);
-      }
-      Derived &operator*=(const Derived &p)
-      {
-        return static_cast<Derived *>(this)->mult_by(p);
-      }
-      Derived operator*(int n) const
-      {
-        Derived retval(*static_cast<Derived const *>(this));
-        retval*=n;
-        return retval;
-      }
-      Derived operator*(const double &x) const
-      {
-        Derived retval(*static_cast<Derived const *>(this));
-        retval*=x;
-        return retval;
-      }
-      Derived operator*(const Derived &p) const
-      {
-        Derived retval(*static_cast<Derived const *>(this));
-        retval*=p;
-        return retval;
-      }
-      Derived &operator/=(int n)
-      {
-        static_cast<Derived *>(this)->generic_division(n);
-        return *static_cast<Derived *>(this);
-      }
-      Derived &operator/=(const double &x)
-      {
-        return *static_cast<Derived *>(this)*=(1./x);
-      }
-      Derived operator/(const double &x) const
-      {
-        Derived retval(*static_cast<Derived const *>(this));
-        retval/=x;
-        return retval;
-      }
-      Derived operator/(int n) const
-      {
-        Derived retval(*static_cast<Derived const *>(this));
-        retval/=n;
-        return retval;
-      }
+      __PIRANHA_BASE_OPERATORS;
 // Complex specifics.
       Derived &operator=(const real_Derived &p)
       {
@@ -273,5 +188,6 @@ namespace piranha
         return retval;
       }
   };
+#undef __PIRANHA_BASE_OPERATORS
 }
 #endif
