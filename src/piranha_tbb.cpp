e872441f2cab56eb1cf6c2db8f152e4751913c72
Francesco Biscani <bluescarni@gmail.com>
2007-12-12 17:48:20 +0100

    Fix include of tbb header.

--- src/piranha_tbb.cpp
+++ src/piranha_tbb.cpp
@@ -18,7 +18,7 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
-#include "piranha_tbb.h"
+#include "bits/piranha_tbb.h"
 
 namespace piranha
 {
