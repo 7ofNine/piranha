efa577ad78bfd555ca40228d4c7727d4cc9a5402
bluescarni <bluescarni@gmail.com>
2008-01-04 23:26:07 +0100

    Reformat.

--- pyranha/Fs/Fs.cpp
+++ pyranha/Fs/Fs.cpp
@@ -22,11 +22,11 @@
 
 BOOST_PYTHON_MODULE(_Fs)
 {
-  class_<fs> inst=ps_basic_instantiation<fs>("fs","Fourier series class.");
+  class_<fs> inst=ps_basic_instantiation<fs>("fs", "Fourier series class.");
   ps_instantiate_differential_specifics(inst);
   ps_instantiate_real_specifics(inst);
   fourier_specifics(inst);
-  def("kep_cosE",&astro::kep_cosE<fs>,"Solve Kepler's equation for cosE.");
+  def("kep_cosE", &astro::kep_cosE<fs>,"Solve Kepler's equation for cosE.");
   def("Pnm",&math::Pnm<fs>,"Legendre function of the first kind - Pnm(cos(theta)).");
   def("Ynm",&math::Ynm<fs>,"Non-normalized spherical harmonic.");
   def("wig_rot",&math::wig_rot<fs>,"Wigner rotation theorem for spherical harmonics.");
