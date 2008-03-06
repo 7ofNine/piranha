a200eaf500cf4068eab12582a517c9dc07ceaeea
bluescarni <bluescarni@gmail.com>
2008-03-06 18:27:41 +0100

    Style fixes.

--- src/core/compile_switches.h
+++ src/core/compile_switches.h
@@ -31,22 +31,22 @@ namespace piranha
   {
     /// Has progress display been built?
     static const bool   display_progress =
-  #ifdef _PIRANHA_DISPLAY_PROGRESS
-      true;
+#ifdef _PIRANHA_DISPLAY_PROGRESS
+    true;
 #else
     false;
 #endif
     /// Has integration with GSL libraries been enabled?
     static const bool   gsl_integration =
-  #ifdef _PIRANHA_GSL
-      true;
+#ifdef _PIRANHA_GSL
+    true;
 #else
     false;
 #endif
     /// Is Piranha using Intel TBB for parallelism?
     static const bool   use_tbb =
-  #ifdef _PIRANHA_TBB
-      true;
+#ifdef _PIRANHA_TBB
+    true;
 #else
     false;
 #endif
