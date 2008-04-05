bc2f1f80ee907d90cdcb0533268347f7944ecd50
bluescarni <bluescarni@gmail.com>
2008-04-05 18:46:16 +0200

    Committed first of new tests and moved a bunch of stuff into the unused directory.

---
+++ tests/fateman_poly_test.cpp
@@ -0,0 +1,69 @@
+/***************************************************************************
+ *   Copyright (C) 2007, 2008 by Francesco Biscani   *
+ *   bluescarni@gmail.com   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#include "../src/manipulators/dpoly.h"
+
+// Fateman's polynomial multiplication test number 1. Calculate:
+// s*(s+1)
+// where s = (1+x+y+z+t)^30
+
+using namespace piranha::manipulators;
+
+int main()
+{
+  dpoly a("fateman_test.dpoly"), b(a);
+  a*=b;
+  a*=b;
+  a*=b;
+  a*=b;
+  a*=b;
+  a*=b;
+  a*=b;
+  a*=b;
+  a*=b;
+  a*=b;
+  a*=b;
+  a*=b;
+  a*=b;
+  a*=b;
+  a*=b;
+  a*=b;
+  a*=b;
+  a*=b;
+  a*=b;
+  a*=b;
+  a*=b;
+  a*=b;
+  a*=b;
+  a*=b;
+  a*=b;
+  a*=b;
+  a*=b;
+  a*=b;
+  a*=b;
+
+  dpoly c(a);
+  c+=1;
+  a*=c;
+
+  std::cout << std::endl << a.nth_index<0>().size() << std::endl;
+
+  return 0;
+}
