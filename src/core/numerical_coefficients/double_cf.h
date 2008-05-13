7d2529dab5c9817001b7195834887052250c1530
bluescarni <bluescarni@gmail.com>
2008-05-13 19:39:38 +0200

    Comment out complex double_cf for now.

--- src/core/numerical_coefficients/double_cf.h
+++ src/core/numerical_coefficients/double_cf.h
@@ -87,72 +87,72 @@ namespace piranha
 }
 
 
-namespace std
-{
-	template <>
-	struct complex<piranha::double_cf>:
-				public piranha::numerical_container<std::complex<double>, complex<piranha::double_cf> >,
-				public piranha::numerical_container_complex_toolbox<piranha::double_cf> {
-private:
-		typedef piranha::numerical_container<std::complex<double>, complex<piranha::double_cf> > ancestor;
-		typedef piranha::numerical_container_complex_toolbox<piranha::double_cf> complex_toolbox;
-		typedef complex self;
-		friend class piranha::numerical_container_complex_toolbox<piranha::double_cf>;
-public:
-		typedef piranha::double_cf value_type;
-// Check if these are needed.
-		/*      using ancestor::swap;
-		      using ancestor::print_plain;
-		      using ancestor::print_latex;
-		      using ancestor::checkup;
-		      using ancestor::invert_sign;
-		      using ancestor::t_eval;
-		      using ancestor::norm;
-		      using ancestor::is_ignorable;
-		      using ancestor::is_insertable;
-		      using ancestor::needs_padding;
-		      using ancestor::pad_right;
-		      using ancestor::apply_layout;
-		      using ancestor::add;
-		      using ancestor::subtract;
-		      using ancestor::mult_by;
-		      using ancestor::mult_by_self;
-		      using ancestor::divide_by;
-		      using complex_toolbox::mult_by;
-		      using complex_toolbox::divide_by;
-		      using complex_toolbox::mult_by_self;
-		      using complex_toolbox::real;
-		      using complex_toolbox::imag;
-		      using complex_toolbox::set_real;
-		      using complex_toolbox::set_imag;*/
-		// Start implementation of basic pseries coefficient interface.
-		//------------
-		// Basic ctors and dtor.
-		explicit complex(): ancestor::numerical_container() {}
-		template <class ArgsTuple>
-		explicit complex(const std::string &s, const ArgsTuple &a): ancestor::numerical_container(s, a) {}
-		template <class ArgsTuple>
-		explicit complex(const int &n, const ArgsTuple &a): ancestor::numerical_container(n, a) {}
-		template <class ArgsTuple>
-		explicit complex(const double &x, const ArgsTuple &a): ancestor::numerical_container(x, a) {}
-		complex(const complex &c): ancestor::numerical_container(c), complex_toolbox::numerical_container_complex_toolbox(c) {}
-		// Complex specific contructors.
-		explicit complex(int r, int i): complex_toolbox::numerical_container_complex_toolbox(r, i) {}
-		explicit complex(const std::complex<int> &c): complex_toolbox::numerical_container_complex_toolbox(c) {}
-		explicit complex(const double &r, const double &i): complex_toolbox::numerical_container_complex_toolbox(r, i) {}
-		explicit complex(const std::complex<double> &c): complex_toolbox::numerical_container_complex_toolbox(c) {}
-		explicit complex(const value_type &r): complex_toolbox::numerical_container_complex_toolbox(r) {}
-		explicit complex(const value_type &r, const value_type &i):
-				complex_toolbox::numerical_container_complex_toolbox(r, i) {}
-		// Operators.
-		self &operator=(const self &val2) {
-			return assign_self(val2);
-		}
-		self &operator=(const value_type &r2) {
-			return assign_self(r2);
-		}
-		// End implementation of complex basic pseries coefficient interface.
-		//------------
-	};
-}
+// namespace std
+// {
+// 	template <>
+// 	struct complex<piranha::double_cf>:
+// 				public piranha::numerical_container<std::complex<double>, complex<piranha::double_cf> >,
+// 				public piranha::numerical_container_complex_toolbox<piranha::double_cf> {
+// private:
+// 		typedef piranha::numerical_container<std::complex<double>, complex<piranha::double_cf> > ancestor;
+// 		typedef piranha::numerical_container_complex_toolbox<piranha::double_cf> complex_toolbox;
+// 		typedef complex self;
+// 		friend class piranha::numerical_container_complex_toolbox<piranha::double_cf>;
+// public:
+// 		typedef piranha::double_cf value_type;
+// // Check if these are needed.
+// 		/*      using ancestor::swap;
+// 		      using ancestor::print_plain;
+// 		      using ancestor::print_latex;
+// 		      using ancestor::checkup;
+// 		      using ancestor::invert_sign;
+// 		      using ancestor::t_eval;
+// 		      using ancestor::norm;
+// 		      using ancestor::is_ignorable;
+// 		      using ancestor::is_insertable;
+// 		      using ancestor::needs_padding;
+// 		      using ancestor::pad_right;
+// 		      using ancestor::apply_layout;
+// 		      using ancestor::add;
+// 		      using ancestor::subtract;
+// 		      using ancestor::mult_by;
+// 		      using ancestor::mult_by_self;
+// 		      using ancestor::divide_by;
+// 		      using complex_toolbox::mult_by;
+// 		      using complex_toolbox::divide_by;
+// 		      using complex_toolbox::mult_by_self;
+// 		      using complex_toolbox::real;
+// 		      using complex_toolbox::imag;
+// 		      using complex_toolbox::set_real;
+// 		      using complex_toolbox::set_imag;*/
+// 		// Start implementation of basic pseries coefficient interface.
+// 		//------------
+// 		// Basic ctors and dtor.
+// 		explicit complex(): ancestor::numerical_container() {}
+// 		template <class ArgsTuple>
+// 		explicit complex(const std::string &s, const ArgsTuple &a): ancestor::numerical_container(s, a) {}
+// 		template <class ArgsTuple>
+// 		explicit complex(const int &n, const ArgsTuple &a): ancestor::numerical_container(n, a) {}
+// 		template <class ArgsTuple>
+// 		explicit complex(const double &x, const ArgsTuple &a): ancestor::numerical_container(x, a) {}
+// 		complex(const complex &c): ancestor::numerical_container(c), complex_toolbox::numerical_container_complex_toolbox(c) {}
+// 		// Complex specific contructors.
+// 		explicit complex(int r, int i): complex_toolbox::numerical_container_complex_toolbox(r, i) {}
+// 		explicit complex(const std::complex<int> &c): complex_toolbox::numerical_container_complex_toolbox(c) {}
+// 		explicit complex(const double &r, const double &i): complex_toolbox::numerical_container_complex_toolbox(r, i) {}
+// 		explicit complex(const std::complex<double> &c): complex_toolbox::numerical_container_complex_toolbox(c) {}
+// 		explicit complex(const value_type &r): complex_toolbox::numerical_container_complex_toolbox(r) {}
+// 		explicit complex(const value_type &r, const value_type &i):
+// 				complex_toolbox::numerical_container_complex_toolbox(r, i) {}
+// 		// Operators.
+// 		self &operator=(const self &val2) {
+// 			return assign_self(val2);
+// 		}
+// 		self &operator=(const value_type &r2) {
+// 			return assign_self(r2);
+// 		}
+// 		// End implementation of complex basic pseries coefficient interface.
+// 		//------------
+// 	};
+// }
 #endif
