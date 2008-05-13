eef74cc07548a1e7bd96f4f000adc9fd825e4bf7
bluescarni <bluescarni@gmail.com>
2008-05-13 19:21:32 +0200

    Generalise a pointer comparison.

--- src/core/base_classes/base_series_math.h
+++ src/core/base_classes/base_series_math.h
@@ -38,7 +38,7 @@ namespace piranha
 	{
 		typedef typename Derived::const_sorted_iterator const_sorted_iterator;
 		typedef typename Derived2::const_sorted_iterator const_sorted_iterator2;
-		p_assert(derived_cast != &s2);
+		p_assert((void *)derived_cast != (void *)&s2);
 		const_sorted_iterator it_hint = derived_const_cast->template nth_index<0>().end();
 		const const_sorted_iterator2 it_f = s2.template nth_index<0>().end();
 		for (const_sorted_iterator2 it = s2.template nth_index<0>().begin(); it != it_f; ++it) {
