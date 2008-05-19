e056f20e0eabaace66b20ddd5d31beda2f225fea
bluescarni <bluescarni@gmail.com>
2008-05-19 16:42:36 +0200

    Add missing operators and handle division by zero in mpq.

--- src/core/mpq.h
+++ src/core/mpq.h
@@ -36,13 +36,18 @@ namespace piranha
 	class mpq:
 				boost::field_operators < mpq,
 				boost::field_operators < mpq, max_fast_int,
-				boost::field_operators < mpq, double
-				> > >
+				boost::field_operators < mpq, double,
+				boost::less_than_comparable< mpq, max_fast_int,
+				boost::less_than_comparable< mpq, double,
+				boost::less_than_comparable< mpq,
+				boost::equality_comparable< mpq, max_fast_int,
+				boost::equality_comparable< mpq
+				> > > > > > > >
 	{
 		public:
-			mpq(): m_value(0) {}
-			mpq(const double &x): m_value(x) {}
-			mpq(const max_fast_int &n, const max_fast_int &d): m_value(n, d) {
+			explicit mpq(): m_value(0) {}
+			explicit mpq(const double &x): m_value(x) {}
+			explicit mpq(const max_fast_int &n, const max_fast_int &d): m_value(n, d) {
 				m_value.canonicalize();
 			}
 // 			mpq(const std::string &s): m_value(0) {
@@ -59,6 +64,27 @@ namespace piranha
 				stream << m_value;
 				std::string retval(stream.str());
 				return retval;
+			}
+			bool operator==(const max_fast_int &n2) const {
+				return m_value == n2;
+			}
+			bool operator==(const mpq &q2) const {
+				return m_value == q2.m_value;
+			}
+			bool operator<(const max_fast_int &n2) const {
+				return m_value < n2;
+			}
+			bool operator<(const double &x2) const {
+				return m_value < x2;
+			}
+			bool operator<(const mpq &q2) const {
+				return m_value < q2.m_value;
+			}
+			bool operator>(const max_fast_int &n2) const {
+				return m_value > n2;
+			}
+			bool operator>(const double &x2) const {
+				return m_value > x2;
 			}
 			mpq &operator+=(const max_fast_int &n) {
 				m_value += n;
@@ -97,23 +123,38 @@ namespace piranha
 				return *this;
 			}
 			mpq &operator/=(const max_fast_int &n) {
+				if (n == 0) {
+					throw division_by_zero();
+				}
 				m_value /= n;
 				return *this;
 			}
 			mpq &operator/=(const double &x) {
+				if (x == 0) {
+					throw division_by_zero();
+				}
 				m_value /= x;
 				return *this;
 			}
 			mpq &operator/=(const mpq &m) {
+				if (m == 0) {
+					throw division_by_zero();
+				}
 				m_value /= m.m_value;
 				return *this;
 			}
 			mpq pow(const max_fast_int &n) const
 			{
 				mpq retval;
-				// If value = 1, then any power is ok, just return 1.
-				if (m_value == 1) {
-					retval.m_value = 1;
+				// If value = 0, then we must make sure we are not dividing by zero.
+				if (m_value == 0) {
+					if (n == 0) {
+						retval.m_value = 1;
+					} else if (n > 0) {
+						retval.m_value = 0;
+					} else {
+						throw division_by_zero();
+					}
 				} else {
 					if (n < 0) {
 						mpz_pow_ui(mpq_denref(retval.m_value.get_mpq_t()), mpq_numref(m_value.get_mpq_t()), (size_t)(-n));
@@ -127,11 +168,21 @@ namespace piranha
 			}
 			mpq pow(const double &x) const
 			{
+				mpq retval;
 				if (m_value == 1) {
-					return mpq(1);
+					retval.m_value = 1;
+				} else if (m_value == 0) {
+					if (x == 0) {
+						retval.m_value = 1;
+					} else if (x > 0) {
+						retval.m_value = 0;
+					} else {
+						throw division_by_zero();
+					}
 				} else {
 					throw unsuitable("Cannot raise rational number to real power.");
 				}
+				return retval;
 			}
 		private:
 			mpq_class	m_value;
