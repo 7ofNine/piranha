833f92fa4886c475f17bd5e895d22feefdf27e61
bluescarni <bluescarni@gmail.com>
2008-07-23 22:10:47 +0200

    Some work on boilerplate for vectorization.

--- src/core/piranha_vectorization.h
+++ src/core/piranha_vectorization.h
@@ -21,9 +21,98 @@
 #ifndef PIRANHA_VECTORIZATION_H
 #define PIRANHA_VECTORIZATION_H
 
+#include "config.h"
+#include "integer_typedefs.h"
+
 #ifdef _PIRANHA_SSE2
 #ifdef __GNUC__
+
 #include <emmintrin.h>
+
+namespace piranha
+{
+	template <int Size>
+	class mfiav_selector
+	{
+			p_static_check(Size == 4, "");
+		public:
+			static const size_t value = 4;
+			static __m128i add(__m128i a, __m128i b) {
+				return _mm_add_epi32(a,b);
+			}
+			static __m128i sub(__m128i a, __m128i b) {
+				return _mm_sub_epi32(a,b);
+			}
+	};
+
+	template <>
+	class mfiav_selector<8>
+	{
+		public:
+			static const size_t value = 2;
+			static __m128i add(__m128i a, __m128i b) {
+				return _mm_add_epi64(a,b);
+			}
+			static __m128i sub(__m128i a, __m128i b) {
+				return _mm_sub_epi64(a,b);
+			}
+	};
+
+	class max_fast_int_atom_vector
+	{
+			typedef mfiav_selector<sizeof(max_fast_int)> selector;
+		public:
+			static const size_t size = selector::value;
+			void add(const max_fast_int_atom_vector &v1, const max_fast_int_atom_vector &v2) {
+				m_container.m = selector::add(v1.m_container.m,v2.m_container.m);
+			}
+			void sub(const max_fast_int_atom_vector &v1, const max_fast_int_atom_vector &v2) {
+				m_container.m = selector::sub(v1.m_container.m,v2.m_container.m);
+			}
+			const max_fast_int &operator[](const size_t &n) const {
+				return m_container.v[n];
+			}
+			max_fast_int &operator[](const size_t &n) {
+				return m_container.v[n];
+			}
+		private:
+			union container_type
+			{
+				max_fast_int	v[size];
+				__m128i			m;
+			};
+			__attribute__ ((aligned (16))) container_type m_container;
+	};
+
+	class double_atom_vector
+	{
+		public:
+			static const size_t size = 2;
+			void mul(const double_atom_vector &v1, const double_atom_vector &v2) {
+				m_container.m = _mm_mul_pd(v1.m_container.m,v2.m_container.m);
+			}
+			void div(const double_atom_vector &v1, const double_atom_vector &v2) {
+				m_container.m = _mm_div_pd(v1.m_container.m,v2.m_container.m);
+			}
+			const double &operator[](const size_t &n) const {
+				return m_container.v[n];
+			}
+			double &operator[](const size_t &n) {
+				return m_container.v[n];
+			}
+		private:
+			union container_type
+			{
+				double	v[size];
+				__m128d	m;
+			};
+			__attribute__ ((aligned (16))) container_type 				m_container;
+			__attribute__ ((aligned (16))) static const container_type	m_v2;
+	};
+
+	extern __attribute__ ((aligned (16))) const double_atom_vector::container_type double_atom_vector::m_v2 = { { 2, 2 } };
+}
+
 #else
 #error No SSE2 support available for this compiler
 #endif // __GNUC__
