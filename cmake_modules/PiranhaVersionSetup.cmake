264cb3b56509965a6ad8817e8ee8701aef3431cd
bluescarni <bluescarni@gmail.com>
2008-08-05 22:58:34 +0200

    Added better versioning system through CMake.

---
+++ cmake_modules/PiranhaVersionSetup.cmake
@@ -0,0 +1,47 @@
+# Copyright (C) 2007, 2008 by Francesco Biscani
+# bluescarni@gmail.com
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the
+# Free Software Foundation, Inc.,
+# 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+# CMake module to setup piranha's version number.
+
+IF(UNIX)
+	SET(DATE_BIN "date")
+	SET(AWK_BIN "awk")
+ELSE(UNIX)
+	# Use the utilities from msys.
+	SET(UTILS_PATH "c:\\msys\\1.0\\bin")
+	SET(DATE_BIN "${UTILS_PATH}\\date.exe")
+	SET(AWK_BIN "${UTILS_PATH}\\gawk.exe")
+ENDIF(UNIX)
+
+EXECUTE_PROCESS(COMMAND ${DATE_BIN} +%Y RESULT_VARIABLE RETVAL
+	OUTPUT_VARIABLE VERSION_MAJOR OUTPUT_STRIP_TRAILING_WHITESPACE)
+EXECUTE_PROCESS(COMMAND ${DATE_BIN} +%m RESULT_VARIABLE RETVAL
+	OUTPUT_VARIABLE VERSION_MINOR OUTPUT_STRIP_TRAILING_WHITESPACE)
+EXECUTE_PROCESS(COMMAND ${DATE_BIN} +%d RESULT_VARIABLE RETVAL
+	OUTPUT_VARIABLE VERSION_PATCH OUTPUT_STRIP_TRAILING_WHITESPACE)
+EXECUTE_PROCESS(COMMAND git --git-dir=${CMAKE_SOURCE_DIR}/.git log -1 --abbrev-commit --pretty=oneline COMMAND
+	${AWK_BIN} -F "." "{print $1}" RESULT_VARIABLE RETVAL
+	OUTPUT_VARIABLE PIRANHA_REV_NUMBER OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+MESSAGE(STATUS "Major version number: ${VERSION_MAJOR}")
+MESSAGE(STATUS "Minor version number: ${VERSION_MINOR}")
+MESSAGE(STATUS "Patch version number: ${VERSION_PATCH}")
+MESSAGE(STATUS "Revision number: ${PIRANHA_REV_NUMBER}")
+
+SET(PIRANHA_VERSION "${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}")
+MESSAGE(STATUS "Piranha version: ${PIRANHA_VERSION}")
--- src/template.settings.cpp
+++ src/template.settings.cpp
@@ -23,7 +23,6 @@
 #include "core/memory.h"
 #include "core/settings.h"
 #include "core/stream_manager.h"
-#include "core/version.h"
 #include "core/piranha_tbb.h" // For task scheduler init.
 
 namespace piranha
@@ -35,10 +34,10 @@ namespace piranha
 	const max_fast_uint settings::max_u = boost::integer_traits<max_fast_uint>::max();
 	const max_fast_int settings::min_i = boost::integer_traits<max_fast_int>::min();
 	const max_fast_int settings::max_i = boost::integer_traits<max_fast_int>::max();
-	std::string settings::m_path = "@THEORIES_INSTALL_PATH@";
-	const std::string settings::m_default_path = "@THEORIES_INSTALL_PATH@";
+	const std::string settings::m_default_path = "@CMAKE_INSTALL_PREFIX@/@THEORIES_INSTALL_PATH@";
+	std::string settings::m_path = settings::m_default_path;
 	bool settings::m_debug = false;
-	const std::string settings::m_version = __PIRANHA_VERSION;
+	const std::string settings::m_version = "@PIRANHA_VERSION@";
 	bool settings::enable_progress_display = true;
 	settings::startup_class settings::startup;
 #ifdef _PIRANHA_TBB
@@ -48,15 +47,16 @@ namespace piranha
 	settings::startup_class::startup_class()
 	{
 		// Startup report.
-		std::cout << "This is Piranha version " << m_version << __PIRANHA_VERSION_TAG << std::endl;
-		std::cout << "Default parameters initialized:" << std::endl;
-		std::cout << "Print precision\t\t\t=\t" << stream_manager::digits() << std::endl;
-		std::cout << "Numerical zero\t\t\t=\t" << numerical_zero() << std::endl;
-		std::cout << "Fast unsigned int range\t\t=\t" << "[0," << max_u << ']' << std::endl;
-		std::cout << "Fast signed int range\t\t=\t" << '[' << min_i << ',' << max_i << ']' << std::endl;
-		std::cout << "Default path\t=\t" << m_default_path << std::endl;
-		std::cout << "Piranha is ready." << std::endl;
-		std::cout << "_______________________________" << std::endl << std::endl;
+		std::cout << "Piranha version: " << m_version << '\n';
+		std::cout << "Revision number: " << "@PIRANHA_REV_NUMBER@\n";
+		std::cout << "Default parameters initialized:\n";
+		std::cout << "Print precision = " << stream_manager::digits() << '\n';
+		std::cout << "Numerical zero = " << numerical_zero() << '\n';
+		std::cout << "Fast unsigned int range = " << "[0," << max_u << ']' << '\n';
+		std::cout << "Fast signed int range = " << '[' << min_i << ',' << max_i << ']' << '\n';
+		std::cout << "Default path = " << m_default_path << '\n';
+		std::cout << "Piranha is ready.\n";
+		std::cout << "_______________________________" << '\n' << '\n';
 		// Setup cout.
 		stream_manager::setup_print(std::cout);
 	}
