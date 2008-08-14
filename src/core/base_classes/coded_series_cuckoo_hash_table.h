4069e84bba411f733314414a7129a1121472f6c8
bluescarni <bluescarni@gmail.com>
2008-08-14 19:13:35 +0200

    Cuckoo hash table is now bucketized.

--- src/core/base_classes/coded_series_cuckoo_hash_table.h
+++ src/core/base_classes/coded_series_cuckoo_hash_table.h
@@ -25,6 +25,7 @@
 #include <vector>
 
 #include "../config.h"
+#include "../math.h"
 #include "../memory.h"
 #include "../p_assert.h"
 #include "../settings.h"
@@ -47,8 +48,29 @@ namespace piranha
 					mutable Cf	m_cf;
 					Ckey		m_ckey;
 			};
-			typedef typename Allocator::template rebind<term_type_>::other allocator_type;
-			typedef std::vector<term_type_,allocator_type> container_type;
+			template <int N>
+			class bucket_type_ {
+					p_static_check(N > 0 && (N == 1 || lg<N>::value > 0), "Invalid bucket size in cuckoo hash.");
+				public:
+					static const size_t size = static_cast<size_t>(N);
+					class flag_bucket_ {
+						public:
+							flag_bucket_() {
+								for (size_t i = 0; i < size; ++i) {
+									f[i] = false;
+								}
+							}
+							bool f[size];
+					};
+					term_type_ t[size];
+			};
+			// Bucket size = 4.
+			typedef bucket_type_<4> bucket_type;
+			static const size_t bsize = bucket_type::size;
+			typedef typename bucket_type::flag_bucket_ flag_bucket_type;
+			typedef typename Allocator::template rebind<bucket_type>::other allocator_type;
+			typedef std::vector<bucket_type,allocator_type> container_type;
+			typedef std::vector<flag_bucket_type,allocator_type> flag_container_type;
 			typedef boost::array<size_t,37> sizes_vector_type;
 			static const sizes_vector_type sizes;
 		public:
@@ -56,31 +78,36 @@ namespace piranha
 			class iterator
 			{
 					friend class coded_series_cuckoo_hash_table;
-					iterator(const coded_series_cuckoo_hash_table *ht): m_ht(ht), m_index(0) {
+					explicit iterator(const coded_series_cuckoo_hash_table *ht): m_ht(ht), m_vindex(0), m_bindex(0) {
 						// Go to the first occupied term if the table is not empty and the first
 						// term is not occupied.
-						if (m_ht->m_container.size() > 0 && !m_ht->m_flags[0]) {
+						if (m_ht->m_container.size() > 0 && !m_ht->m_flags[0].f[0]) {
 							next();
 						}
 					}
-					iterator(const coded_series_cuckoo_hash_table *ht, const size_t &pos): m_ht(ht), m_index(pos) {}
+					explicit iterator(const coded_series_cuckoo_hash_table *ht, const size_t &vpos, const size_t &bpos):
+						m_ht(ht), m_vindex(vpos), m_bindex(bpos) {
+						p_assert(m_bindex < bsize);
+					}
 				public:
 					iterator &operator++() {
 						next();
 						return *this;
 					}
 					const term_type &operator*() const {
-						p_assert(m_index < m_ht->m_container.size());
-						p_assert(m_ht->m_flags[m_index]);
-						return m_ht->m_container[m_index];
+						p_assert(m_vindex < m_ht->m_container.size());
+						p_assert(m_bindex < bsize);
+						p_assert(m_ht->m_flags[m_vindex].f[m_bindex]);
+						return m_ht->m_container[m_vindex].t[m_bindex];
 					}
 					const term_type *operator->() const {
-						p_assert(m_index < m_ht->m_container.size());
-						p_assert(m_ht->m_flags[m_index]);
-						return &m_ht->m_container[m_index];
+						p_assert(m_vindex < m_ht->m_container.size());
+						p_assert(m_bindex < bsize);
+						p_assert(m_ht->m_flags[m_vindex].f[m_bindex]);
+						return &m_ht->m_container[m_vindex].t[m_bindex];
 					}
 					bool operator==(const iterator &it2) const {
-						return (m_ht == it2.m_ht && m_index == it2.m_index);
+						return (m_ht == it2.m_ht && m_vindex == it2.m_vindex && m_bindex == it2.m_bindex);
 					}
 					bool operator!=(const iterator &it2) const {
 						return !(*this == it2);
@@ -88,15 +115,22 @@ namespace piranha
 				private:
 					void next() {
 						const size_t vector_size = m_ht->m_container.size();
-						size_t tmp_index = m_index;
+						size_t tmp_vindex = m_vindex, tmp_bindex = m_bindex;
 						do {
-							++tmp_index;
-						} while (tmp_index < vector_size && !m_ht->m_flags[tmp_index]);
-						m_index = tmp_index;
+							if (tmp_bindex == bsize - 1) {
+								tmp_bindex = 0;
+								++tmp_vindex;
+							} else {
+								++tmp_bindex;
+							}
+						} while (tmp_vindex < vector_size && !m_ht->m_flags[tmp_vindex].f[tmp_bindex]);
+						m_vindex = tmp_vindex;
+						m_bindex = tmp_bindex;
 					}
 				private:
 					const coded_series_cuckoo_hash_table	*m_ht;
-					size_t									m_index;
+					size_t									m_vindex;
+					size_t									m_bindex;
 			};
 			coded_series_cuckoo_hash_table(): m_length(0), m_sizes_index(0),
 				m_container(sizes[0]), m_flags(sizes[0]) {}
@@ -109,26 +143,33 @@ namespace piranha
 			}
 			iterator end() const {
 				p_assert(sizes[m_sizes_index] <= m_container.size());
-				return iterator(this, m_container.size());
+				return iterator(this, m_container.size(), 0);
 			}
 			iterator find(const Ckey &ckey) const {
 				p_assert(sizes[m_sizes_index] <= m_container.size());
+				p_assert(sizes[m_sizes_index] <= m_flags.size());
 				const size_t pos1 = position1(ckey);
 				// TODO: replace with bit twiddling to reduce branching?
-				if (m_flags[pos1] && m_container[pos1].m_ckey == ckey) {
-					return iterator(this,pos1);
+				for (size_t i = 0; i < bsize; ++i) {
+					if (m_flags[pos1].f[i] && m_container[pos1].t[i].m_ckey == ckey) {
+						return iterator(this,pos1,i);
+					}
 				}
 				const size_t pos2 = position2(ckey);
-				if (m_flags[pos2] && m_container[pos2].m_ckey == ckey) {
-					return iterator(this,pos2);
+				for (size_t i = 0; i < bsize; ++i) {
+					if (m_flags[pos2].f[i] && m_container[pos2].t[i].m_ckey == ckey) {
+						return iterator(this,pos2,i);
+					}
 				}
-				return find_among_bad_terms(ckey);
+				//return find_among_bad_terms(ckey);
+				return end();
 			}
 			size_t size() const {
 				return m_length;
 			}
 			void insert(const term_type &t) {
-				if (((m_length + 1) << 1) >= sizes[m_sizes_index]) {
+				//if (((m_length + 1) << 1) >= sizes[m_sizes_index] * bsize) {
+				if (((double)m_length + 1) / (sizes[m_sizes_index] * bsize) >= .5) {
 					__PDEBUG(std::cout << "Max load factor exceeded, resizing." << '\n');
 					increase_size();
 				}
@@ -144,15 +185,15 @@ namespace piranha
 				}
 			}
 		private:
-			iterator find_among_bad_terms(const Ckey &ckey) const {
-				const size_t size = m_container.size();
-				for (size_t i = sizes[m_sizes_index]; i < size; ++i) {
-					if (m_flags[i] && m_container[i].m_ckey == ckey) {
-						return iterator(this,i);
-					}
-				}
-				return end();
-			}
+// 			iterator find_among_bad_terms(const Ckey &ckey) const {
+// 				const size_t size = m_container.size();
+// 				for (size_t i = sizes[m_sizes_index]; i < size; ++i) {
+// 					if (m_flags[i] && m_container[i].m_ckey == ckey) {
+// 						return iterator(this,i);
+// 					}
+// 				}
+// 				return end();
+// 			}
 			void increase_size() {
 				coded_series_cuckoo_hash_table new_ht;
 				new_ht.m_sizes_index = m_sizes_index + 1;
@@ -183,54 +224,67 @@ namespace piranha
 			}
 			bool attempt_insertion(const term_type &t, term_type &tmp_term) {
 				size_t pos = position1(t.m_ckey);
-				if (m_flags[pos]) {
-					// Current term is kicked out into tmp_term, t takes its place.
-					tmp_term = m_container[pos];
-					m_container[pos] = t;
-					size_t counter = 0;
-					while (swap_and_displace(tmp_term,pos)) {
-						++counter;
-						if (counter > 20) {
-							__PDEBUG(std::cout << "Cuckoo loop detected, will mark term as bad.\n");
-							return append_as_bad_term(tmp_term);
-						}
-					}
-//std::cout << "broke out at counter " << counter << '\n';
-				} else {
-					m_flags[pos] = true;
-					m_container[pos] = t;
-					++m_length;
-				}
-				return true;
-			}
-			bool append_as_bad_term(term_type &t) {
-				// Maybe there is a non-occupied bad slot we can re-use?
-				const size_t size = m_container.size();
-				for (size_t i = sizes[m_sizes_index]; i < size; ++i) {
-					if (!m_flags[i]) {
-						m_container[i].swap(t);
-						m_flags[i] = true;
+				for (size_t i = 0; i < bsize; ++i) {
+					// There's space in the bucket, rejoice!
+					if (!m_flags[pos].f[i]) {
+						m_flags[pos].f[i] = true;
+						m_container[pos].t[i] = t;
 						++m_length;
 						return true;
 					}
 				}
-				// We did not find a non-occupied bad slot, create one if we are below the limit and copy the
-				// term, otherwise give up and return false.
-				if ((m_container.size() - sizes[m_sizes_index]) < 5) {
-					m_container.push_back(term_type());
-					m_container.back().swap(t);
-					m_flags.push_back(true);
-					++m_length;
-					return true;
+				// No space was found in the first-choice bucket. Choose randomly(?) the index of the element
+				// in the bucket that will be displaced.
+				size_t dindex;
+				if (bsize == 1) {
+					// If we have single-term bucket, result will always be 0.
+					dindex = 0;
+				} else {
+					// We know bucket size is power of 2 when it is not 1, use bit twiddling for mod.
+					dindex = (pos & (bsize - 1));
 				}
-				__PDEBUG(std::cout << "There are already too many bad terms, failing insertion.\n");
-				return false;
+				tmp_term = m_container[pos].t[dindex];
+				m_container[pos].t[dindex] = t;
+				size_t counter = 0;
+				while (swap_and_displace(tmp_term,pos)) {
+					++counter;
+					if (counter > 10) {
+						__PDEBUG(std::cout << "Cuckoo loop detected, returning false: " <<
+							(((double)m_length + 1) / (sizes[m_sizes_index] * bsize)) << '\n');
+						return false;
+					}
+				}
+				return true;
 			}
+// 			bool append_as_bad_term(term_type &t) {
+// 				// Maybe there is a non-occupied bad slot we can re-use?
+// 				const size_t size = m_container.size();
+// 				for (size_t i = sizes[m_sizes_index]; i < size; ++i) {
+// 					if (!m_flags[i]) {
+// 						m_container[i].swap(t);
+// 						m_flags[i] = true;
+// 						++m_length;
+// 						return true;
+// 					}
+// 				}
+// 				// We did not find a non-occupied bad slot, create one if we are below the limit and copy the
+// 				// term, otherwise give up and return false.
+// 				if ((m_container.size() - sizes[m_sizes_index]) < 5) {
+// 					m_container.push_back(term_type());
+// 					m_container.back().swap(t);
+// 					m_flags.push_back(true);
+// 					++m_length;
+// 					return true;
+// 				}
+// 				__PDEBUG(std::cout << "There are already too many bad terms, failing insertion.\n");
+// 				return false;
+// 			}
 			// Place tmp_term into its location other than orig_location, displacing, if necessary. an existing
 			// term. If displacement takes place, retval will be true and the content of tmp_term
 			// will be the displaced one. Otherwise return false.
 			bool swap_and_displace(term_type &tmp_term, size_t &orig_location) {
 				//__PDEBUG(std::cout << "Performing swap & displace." << '\n');
+				// First thing we need to know if the original location was given by hash1 or hash2.
 				const size_t pos1 = position1(tmp_term.m_ckey);
 				size_t new_pos;
 				if (orig_location == pos1) {
@@ -240,25 +294,45 @@ namespace piranha
 					// Original location was pos2, we want to move to pos1.
 					new_pos = pos1;
 				}
-				if (m_flags[new_pos]) {
-					// We have to displace an existing term.
-					m_container[new_pos].swap(tmp_term);
-					orig_location = new_pos;
-					return true;
-				} else {
-					// Destination is not taken, occupy it.
-					m_container[new_pos].swap(tmp_term);
-					m_flags[new_pos] = true;
-					return false;
+				for (size_t i = 0; i < bsize; ++i) {
+					if (!m_flags[new_pos].f[i]) {
+						// Place found, rejoice!
+						m_container[new_pos].t[i].swap(tmp_term);
+						m_flags[new_pos].f[i] = true;
+						return false;
+					}
 				}
+				// No space was found in the first-choice bucket. Choose randomly(?) the index of the element
+				// in the bucket that will be displaced.
+				size_t dindex;
+				if (bsize == 1) {
+					// If we have single-term bucket, result will always be 0.
+					dindex = 0;
+				} else {
+					// We know bucket size is power of 2 when it is not 1, use bit twiddling for mod.
+					dindex = (new_pos & (bsize - 1));
+				}
+				// Displace the selected term.
+				m_container[new_pos].t[dindex].swap(tmp_term);
+				orig_location = new_pos;
+				return true;
 			}
 			size_t position1(const Ckey &ckey) const {
 				return static_cast<size_t>(ckey) % sizes[m_sizes_index];
+
+				size_t retval = static_cast<size_t>(ckey);
+				retval *= (retval + sizes[m_sizes_index]);
+				return retval % sizes[m_sizes_index];
 			}
 			size_t position2(const Ckey &ckey) const {
 				size_t seed = static_cast<size_t>(ckey);
 				seed ^= seed + 0x9e3779b9 + (seed << 6) + (seed >> 2);
 				return seed % sizes[m_sizes_index];
+
+// 				size_t seed = static_cast<size_t>(ckey);
+// 				seed ^= seed + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+// 				return (seed * (seed + sizes[m_sizes_index])) % sizes[m_sizes_index];
+
 				// This is Jenkins' hash.
 // 				size_t key = (~ckey) + (ckey << 21); // key = (key << 21) - key - 1;
 // 				key = key ^ (key >> 24);
@@ -274,7 +348,7 @@ namespace piranha
 			size_t				m_length;
 			size_t				m_sizes_index;
 			container_type		m_container;
-			std::vector<char>	m_flags;
+			flag_container_type	m_flags;
 	};
 
 	template <class Cf, class Ckey, class Allocator>
