f9eb1d81626d7a96e53e85d01879f0bd8f3c9b8d
bluescarni <bluescarni@gmail.com>
2008-09-01 15:58:47 +0200

    Use plain pointer as internal container for cuckoo hash instead of std::vector.
    We save some bytes (useful for small series when cuckoo will become the backbone container),
    and apparently the compiler optimizes better.

--- src/core/base_classes/coded_series_cuckoo_hash_table.h
+++ src/core/base_classes/coded_series_cuckoo_hash_table.h
@@ -21,7 +21,7 @@
 #ifndef PIRANHA_CODED_SERIES_CUCKOO_HASH_TABLE_H
 #define PIRANHA_CODED_SERIES_CUCKOO_HASH_TABLE_H
 
-#include <algorithm> // For std::max.
+#include <algorithm> // For std::max and std::swap.
 #include <exception> // For std::bad_alloc.
 #include <utility> // For std::pair.
 #include <vector>
@@ -78,7 +78,6 @@ namespace piranha
 			p_static_check(mults_size % 2 == 0, "Mults size must be a multiple of 2.");
 			static const size_t bsize = bucket_type::size;
 			typedef typename Allocator::template rebind<bucket_type>::other allocator_type;
-			typedef std::vector<bucket_type,allocator_type> container_type;
 		public:
 			typedef term_type_ term_type;
 			class iterator
@@ -87,7 +86,7 @@ namespace piranha
 					explicit iterator(const coded_series_cuckoo_hash_table *ht): m_ht(ht), m_vindex(0), m_bindex(0) {
 						// Go to the first occupied term if the table is not empty and the first
 						// term is not occupied.
-						if (m_ht->m_container.size() > 0 && !m_ht->m_container[0].f[0]) {
+						if (sizes[m_ht->m_sizes_index] > 0 && !m_ht->m_container[0].f[0]) {
 							next();
 						}
 					}
@@ -101,13 +100,13 @@ namespace piranha
 						return *this;
 					}
 					const term_type &operator*() const {
-						p_assert(m_vindex < m_ht->m_container.size());
+						p_assert(m_vindex < sizes[m_ht->m_sizes_index]);
 						p_assert(m_bindex < bsize);
 						p_assert(m_ht->m_container[m_vindex].f[m_bindex]);
 						return m_ht->m_container[m_vindex].t[m_bindex];
 					}
 					const term_type *operator->() const {
-						p_assert(m_vindex < m_ht->m_container.size());
+						p_assert(m_vindex < sizes[m_ht->m_sizes_index]);
 						p_assert(m_bindex < bsize);
 						p_assert(m_ht->m_container[m_vindex].f[m_bindex]);
 						return &m_ht->m_container[m_vindex].t[m_bindex];
@@ -120,7 +119,8 @@ namespace piranha
 					}
 				private:
 					void next() {
-						const size_t vector_size = m_ht->m_container.size();
+						const size_t vector_size = sizes[m_ht->m_sizes_index];
+						bucket_type *container = m_ht->m_container;
 						size_t tmp_vindex = m_vindex, tmp_bindex = m_bindex;
 						do {
 							if (tmp_bindex == bsize - 1) {
@@ -129,7 +129,7 @@ namespace piranha
 							} else {
 								++tmp_bindex;
 							}
-						} while (tmp_vindex < vector_size && !m_ht->m_container[tmp_vindex].f[tmp_bindex]);
+						} while (tmp_vindex < vector_size && !container[tmp_vindex].f[tmp_bindex]);
 						m_vindex = tmp_vindex;
 						m_bindex = tmp_bindex;
 					}
@@ -139,26 +139,24 @@ namespace piranha
 					size_t									m_bindex;
 			};
 			typedef iterator const_iterator;
-			coded_series_cuckoo_hash_table(): m_sizes_index(0), m_mults_index(0), m_length(0) {
-				m_container.resize(sizes[2]);
-				m_sizes_index = 2;
+			coded_series_cuckoo_hash_table(): m_sizes_index(2), m_mults_index(0), m_length(0) {
+				init();
 			}
 			coded_series_cuckoo_hash_table(const size_t &size): m_mults_index(0), m_length(0) {
-				uint8 sizes_index = find_upper_pow2_index(size / bsize);
-				p_assert(sizes_index >= 2);
+				m_sizes_index = find_upper_pow2_index(size / bsize);
+				p_assert(m_sizes_index >= 2);
 				while (true) {
 					try {
-						m_container.resize(sizes[sizes_index]);
-						m_sizes_index = sizes_index;
+						init();
 						break;
 					} catch (const out_of_memory &) {
-						--sizes_index;
-						if (sizes_index < 2) {
+						--m_sizes_index;
+						if (m_sizes_index < 2) {
 							throw out_of_memory("Not enough available memory to allocate a cuckoo hash table of size 4.");
 						}
 					} catch (const std::bad_alloc &) {
-						--sizes_index;
-						if (sizes_index < 2) {
+						--m_sizes_index;
+						if (m_sizes_index < 2) {
 							throw out_of_memory("Not enough physical memory to allocate a cuckoo hash table of size 4.");
 						}
 					}
@@ -171,44 +169,40 @@ namespace piranha
 					++i;
 				}
 				p_assert(i == size());
-				std::cout << "No problems. Final vector size: "<< m_container.size() << '\n';
+				std::cout << "No problems. Final vector size: "<< sizes[m_sizes_index] << '\n';
 				)
-				p_assert(sizes[m_sizes_index] == m_container.size());
+				destroy();
 			}
 			iterator begin() const {
-				p_assert(sizes[m_sizes_index] == m_container.size());
 				return iterator(this);
 			}
 			iterator end() const {
-				p_assert(sizes[m_sizes_index] == m_container.size());
-				return iterator(this, m_container.size(), 0);
+				return iterator(this, sizes[m_sizes_index], 0);
 			}
 			iterator find(const Ckey &ckey) const {
-				p_assert(sizes[m_sizes_index] == m_container.size());
 				const size_t h = static_cast<size_t>(ckey), pos1 = position1(h), pos2 = position2(h);
+				bucket_type *container = m_container;
 				// TODO: replace with bit twiddling to reduce branching?
 				for (size_t i = 0; i < bsize; ++i) {
-					if (m_container[pos1].f[i] && m_container[pos1].t[i].m_ckey == ckey) {
+					if (container[pos1].f[i] && container[pos1].t[i].m_ckey == ckey) {
 						return iterator(this,pos1,i);
 					}
 				}
 				for (size_t i = 0; i < bsize; ++i) {
-					if (m_container[pos2].f[i] && m_container[pos2].t[i].m_ckey == ckey) {
+					if (container[pos2].f[i] && container[pos2].t[i].m_ckey == ckey) {
 						return iterator(this,pos2,i);
 					}
 				}
 				return end();
 			}
 			size_t size() const {
-				p_assert(sizes[m_sizes_index] == m_container.size());
 				return m_length;
 			}
 			bool empty() const {
 				return (m_length == 0);
 			}
 			void insert(const term_type &t) {
-				if ((static_cast<double>(m_length) + 1) >=
-					settings::load_factor() * (sizes[m_sizes_index] * bsize)) {
+				if ((static_cast<double>(m_length) + 1) >= settings::load_factor() * (sizes[m_sizes_index] * bsize)) {
 					__PDEBUG(std::cout << "Max load factor exceeded, resizing." << '\n');
 					grow();
 				}
@@ -224,16 +218,40 @@ namespace piranha
 					// it could be turned into an iteration with some effort?
 					insert(tmp_term);
 				}
-				p_assert(sizes[m_sizes_index] == m_container.size());
 			}
 			void swap(coded_series_cuckoo_hash_table &other) {
 				int_swap(m_mults_index,other.m_mults_index);
 				int_swap(m_sizes_index,other.m_sizes_index);
 				int_swap(m_length,other.m_length);
-				m_container.swap(other.m_container);
-				p_assert(sizes[m_sizes_index] == m_container.size());
+				std::swap(m_container,other.m_container);
 			}
 		private:
+			// Allocate and construct buckets according to the size specified by m_sizes_index.
+			void init() {
+				allocator_type a;
+				m_container = a.allocate(sizes[m_sizes_index]);
+				bucket_type *container = m_container;
+				const bucket_type bucket;
+				for (size_t i =  0; i < sizes[m_sizes_index]; ++i) {
+					a.construct(container + i,bucket);
+				}
+			}
+			// Destroy and deallocate buckets according to the size specified by m_sizes_index.
+			void destroy() {
+				allocator_type a;
+				bucket_type *container = m_container;
+				for (size_t i =  0; i < sizes[m_sizes_index]; ++i) {
+					a.destroy(container + i);
+				}
+				a.deallocate(container,sizes[m_sizes_index]);
+			}
+			// Resize to new sizes index, destroying everything in the container.
+			void resize(const uint8 &new_sizes_index) {
+				destroy();
+				m_sizes_index = new_sizes_index;
+				m_length = 0;
+				init();
+			}
 			static uint8 find_upper_pow2_index(const size_t &size) {
 				for (uint8 retval = 0; retval < sizes_size; ++retval) {
 					if (sizes[retval] >= size) {
@@ -244,8 +262,7 @@ namespace piranha
 			}
 			void grow() {
 				coded_series_cuckoo_hash_table new_ht;
-				new_ht.m_container.resize(sizes[m_sizes_index + 1]);
-				new_ht.m_sizes_index = m_sizes_index + 1;
+				new_ht.resize(m_sizes_index + 1);
 				// Assign current mults to the new table.
 				new_ht.m_mults_index = m_mults_index;
 				term_type tmp_term;
@@ -253,31 +270,22 @@ namespace piranha
 				const iterator it_f = end();
 				while (it != it_f) {
 					if (!new_ht.attempt_insertion(*it,tmp_term)) {
-						// TODO: here we should check with other hash functions before giving up and increasing size.
+						// TODO: here should we check with other hash functions before giving up and increasing size?
 						__PDEBUG(std::cout << "Cuckoo hash table resize triggered during resize." << '\n');
 						const size_t new_index = new_ht.m_sizes_index + 1;
 						__PDEBUG(std::cout << "Next size: " << sizes[new_index] << '\n');
-						// We do things in this order because if resizing fails due to insufficient memory,
-						// then we must be sure that the size index is consistent with the initial size (otherwise
-						// assertions can fail in the dtor, for example).
-						new_ht.m_container.clear();
-						new_ht.m_sizes_index = 0;
-						new_ht.m_container.resize(sizes[new_index]);
-						new_ht.m_sizes_index = new_index;
-						new_ht.m_length = 0;
+						new_ht.resize(new_index);
 						it = begin();
 					} else {
 						++it;
 					}
 				}
 				swap(new_ht);
-				p_assert(sizes[m_sizes_index] == m_container.size());
 			}
 			bool rehash() {
 				coded_series_cuckoo_hash_table new_ht;
 				new_ht.m_mults_index = m_mults_index;
-				new_ht.m_container.resize(sizes[m_sizes_index]);
-				new_ht.m_sizes_index = m_sizes_index;
+				new_ht.resize(m_sizes_index);
 				for (size_t i = 0; i < max_rehash_tries; ++i) {
 					term_type tmp_term;
 					bool success = true;
@@ -295,48 +303,42 @@ namespace piranha
 						__PDEBUG(std::cout << "Rehash successful after " << (i + 1) << " tries.\n");
 						// This means that we were able to insert all terms. Swap and return true.
 						swap(new_ht);
-						p_assert(sizes[m_sizes_index] == m_container.size());
 						return true;
 					}
 					__PDEBUG(std::cout << "Rehash stopped at " << new_ht.size() << " out of " << size() << ".\n");
 					// Let's reset new_ht.
-					new_ht.m_container.clear();
-					new_ht.m_container.resize(sizes[m_sizes_index]);
-					new_ht.m_length = 0;
+					new_ht.resize(m_sizes_index);
 				}
 				__PDEBUG(std::cout << "Mults exhausted, rehash failed.\n")
-				p_assert(sizes[m_sizes_index] == m_container.size());
 				return false;
 			}
 			bool attempt_insertion(const term_type &t, term_type &tmp_term) {
 				const size_t h = static_cast<size_t>(t.m_ckey);
 				size_t pos = position1(h);
+				bucket_type *container = m_container;
 				for (size_t i = 0; i < bsize; ++i) {
 					// There's space in the bucket, rejoice!
-					if (!m_container[pos].f[i]) {
-						m_container[pos].f[i] = true;
-						m_container[pos].t[i] = t;
+					if (!container[pos].f[i]) {
+						container[pos].f[i] = true;
+						container[pos].t[i] = t;
 						++m_length;
-						p_assert(sizes[m_sizes_index] == m_container.size());
 						return true;
 					}
 				}
 				// No space was found in the first-choice bucket. Choose randomly(?) the index of the element
 				// in the bucket that will be displaced.
 				const size_t dindex = (bsize == 1) ? 0 : (pos & (bsize - 1));
-				tmp_term = m_container[pos].t[dindex];
-				m_container[pos].t[dindex] = t;
+				tmp_term = container[pos].t[dindex];
+				container[pos].t[dindex] = t;
 				size_t counter = 0;
 				while (swap_and_displace(tmp_term,pos)) {
 					++counter;
 					if (counter > 10) {
 						__PDEBUG(std::cout << "Cuckoo loop detected, returning false. Load factor is: " <<
 							((static_cast<double>(m_length) + 1) / (sizes[m_sizes_index] * bsize)) << '\n');
-						p_assert(sizes[m_sizes_index] == m_container.size());
 						return false;
 					}
 				}
-				p_assert(sizes[m_sizes_index] == m_container.size());
 				return true;
 			}
 			// Place tmp_term into its location other than orig_location, displacing, if necessary. an existing
@@ -346,13 +348,13 @@ namespace piranha
 				// First thing we need to know if the original location was given by hash1 or hash2.
 				const size_t h = static_cast<size_t>(tmp_term.m_ckey), pos1 = position1(h),
 					new_pos = (orig_location == pos1) ? position2(h) : pos1;
+				bucket_type *container = m_container;
 				for (size_t i = 0; i < bsize; ++i) {
-					if (!m_container[new_pos].f[i]) {
+					if (!container[new_pos].f[i]) {
 						// Place found, rejoice!
-						m_container[new_pos].t[i].swap(tmp_term);
-						m_container[new_pos].f[i] = true;
+						container[new_pos].t[i].swap(tmp_term);
+						container[new_pos].f[i] = true;
 						++m_length;
-						p_assert(sizes[m_sizes_index] == m_container.size());
 						return false;
 					}
 				}
@@ -360,9 +362,8 @@ namespace piranha
 				// in the bucket that will be displaced.
 				const size_t dindex = (bsize == 1) ? 0 : (new_pos & (bsize - 1));
 				// Displace the selected term.
-				m_container[new_pos].t[dindex].swap(tmp_term);
+				container[new_pos].t[dindex].swap(tmp_term);
 				orig_location = new_pos;
-				p_assert(sizes[m_sizes_index] == m_container.size());
 				return true;
 			}
 			static size_t m_hash(const size_t &h, const size_t &mult, const size_t &sizes_index) {
@@ -386,7 +387,7 @@ namespace piranha
 			uint8				m_sizes_index;
 			uint8				m_mults_index;
 			size_t				m_length;
-			container_type		m_container;
+			bucket_type			*m_container;
 	};
 
 	template <class Cf, class Ckey, class Allocator>
