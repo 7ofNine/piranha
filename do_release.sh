780df3c8b54b3325d3e4c8e41e64ea807c3195ff
bluescarni <bluescarni@gmail.com>
2008-09-09 21:15:40 +0200

    Add first version of script to do releases.
    Actually it will just update the Changelog.

---
+++ do_release.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+CURRENT_RELEASE=`git log --pretty=oneline HEAD^1..HEAD|awk '{print $1}'`
+PREVIOUS_RELEASE=`cat current_release`
+cp Changelog Changelog.tmp
+echo "`date +%Y.%m.%d` ${CURRENT_RELEASE}" > Changelog
+echo "" >> Changelog
+git log --pretty=oneline ${PREVIOUS_RELEASE}.. | grep -i "clog:" | awk -F "clog:" '{print $2}' | sed 's/^[ \t]*//;s/[ \t]*$//'|sed 's/^/\*\ /' >> Changelog
+echo "" >> Changelog
+cat Changelog.tmp >> Changelog
+echo ${CURRENT_RELEASE} > current_release
+rm Changelog.tmp
