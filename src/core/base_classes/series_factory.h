847d571c0acec468b98d6931f5068a5f457151d9
Francesco Biscani <francesco.biscani@esa.int>
2009-05-05 17:23:36 +0200

    Added file for construction of series from cf/keys.

---
+++ src/core/base_classes/series_factory.h
@@ -0,0 +1,60 @@
+/***************************************************************************
+ *   Copyright (C) 2007, 2008 by Francesco Biscani   *
+ *   bluescarni@gmail.com   *
+ *                                                                         *
+ *   This program is free software; you can redis\bute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#ifndef PIRANHA_SERIES_FACTORY_H
+#define PIRANHA_SERIES_FACTORY_H
+
+namespace piranha
+{
+	template <class RequestedKey, class SeriesKey>
+	struct series_from_key_impl
+	{
+		template <class Series, class ArgsTuple>
+		static void run(Series &retval, const RequestedKey &key, const ArgsTuple &args_tuple)
+		{
+			typedef typename Series::term_type::cf_type cf_series_type;
+			cf_series_type cf_series;
+			series_from_key_impl<RequestedKey,typename cf_series_type::term_type::key_type>::run(
+				cf_series,key,args_tuple);
+			retval.insert(typename Series::term_type(cf_series,typename Series::term_type::key_type()),args_tuple);
+		}
+	};
+
+	template <class Key>
+	struct series_from_key_impl<Key,Key>
+	{
+		template <class Series, class ArgsTuple>
+		static void run(Series &retval, const Key &key, const ArgsTuple &args_tuple)
+		{
+			retval.insert(typename Series::term_type(typename Series::term_type::cf_type(1,args_tuple),key),args_tuple);
+		}
+	};
+
+	template <class Series, class Key, class ArgsTuple>
+	Series series_from_key(const Key &key, const ArgsTuple &args_tuple)
+	{
+		typedef typename Series::term_type term_type;
+		Series retval;
+		series_from_key_impl<Key, typename term_type::key_type>::run(retval,key,args_tuple);
+		return retval;
+	}
+}
+
+#endif
