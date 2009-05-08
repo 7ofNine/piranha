cccb6f39a58f983a052dc33cc86f4676a613a3d0
Francesco Biscani <francesco.biscani@esa.int>
2009-05-08 19:25:26 +0200

    Split mp_rational into generic container, to be used in the future also for mpz.

--- src/core/mp/piranha_gmp.h
+++ src/core/mp/piranha_gmp.h
@@ -21,10 +21,8 @@
 #ifndef PIRANHA_PIRANHA_GMP_H
 #define PIRANHA_PIRANHA_GMP_H
 
-#include <algorithm>
 #include <boost/algorithm/string.hpp>
 #include <boost/functional/hash.hpp>
-#include <boost/operators.hpp>
 #include <cmath>
 #include <complex>
 #include <exception>
@@ -36,58 +34,30 @@
 
 #include "../exceptions.h"
 #include "../utils.h"
+#include "generic_mp_container.h"
 
 namespace piranha
 {
-	// Macros for operators.
-	#define EQUALITY_OPERATOR(type) \
-	/** \brief Test equality with type. */ \
-	bool operator==(const type &other) const \
+	#define ASSIGNMENT_OPERATOR(self, type) \
+	/** \brief Assignment operator for type. */ \
+	self &operator=(const type &x) \
 	{ \
-		return (m_value == other); \
-	}
-	#define COMPARISON_OPERATOR(type) \
-	/** \brief Comparison operator with type. */ \
-	bool operator<(const type &other) const \
-	{ \
-		return (m_value < other); \
-	}
-	#define INPLACE_OPERATOR(self,op,type) \
-	/** \brief In-place operator op for type. */ \
-	self &operator op(const type &other) \
-	{ \
-		m_value op other; \
-		return *this; \
-	}
-	#define INPLACE_DIVISION(self,type) \
-	/** \brief In-place division with type. */ \
-	self &operator/=(const type &other) \
-	{ \
-		if (other == 0) { \
-			piranha_throw(zero_division_error,"cannot divide by zero"); \
-		} \
-		m_value /= other; \
-		return *this; \
+		return this->assign(x); \
 	}
 
 	/// Multiprecision rational class.
 	/**
-	 * Wraps a GMP mpq_class. Interoperability with C++ int and double types is provided through constructors and
-	 * in-place mathematical operators. Non in-place operators are provided through inheritance from the Boost operator library.
+	 * Wraps a GMP mpq_class.
 	 */
-	class mp_rational:
-		boost::ordered_field_operators<mp_rational,
-		boost::ordered_field_operators<mp_rational, int,
-		boost::ordered_field_operators<mp_rational, double
-		> > >
+	class mp_rational: public generic_mp_container<mpq_class,mp_rational>
 	{
-			friend std::ostream &operator<<(std::ostream &, const mp_rational &);
+			typedef generic_mp_container<mpq_class,mp_rational> ancestor;
 		public:
 			/// Default constructor.
 			/**
 			 * Initialises value to zero.
 			 */
-			explicit mp_rational():m_value(0) {}
+			explicit mp_rational():ancestor() {}
 			/// Constructor from std::string.
 			/**
 			 * Will raise a value_error exception if string is not valid. Valid strings include
@@ -95,7 +65,7 @@ namespace piranha
 			 * @throws value_error if string is invalid.
 			 * @throws zero_division_error if string is valid but denominator is zero.
 			 */
-			explicit mp_rational(const std::string &str):m_value()
+			explicit mp_rational(const std::string &str):ancestor()
 			{
 				construct_from_string(str.c_str());
 			}
@@ -103,17 +73,17 @@ namespace piranha
 			/**
 			 * @see mp_rational(const std::string &).
 			 */
-			explicit mp_rational(const char *str):m_value()
+			explicit mp_rational(const char *str):ancestor()
 			{
 				construct_from_string(str);
 			}
 			/// Constructor from integer.
-			explicit mp_rational(const int &n):m_value(n) {}
+			explicit mp_rational(const int &n):ancestor(n) {}
 			/// Constructor from integer numerator and denominator.
 			/**
 			 * @throws zero_division_error if denominator is zero.
 			 */
-			explicit mp_rational(const int &n, const int &d):m_value()
+			explicit mp_rational(const int &n, const int &d):ancestor()
 			{
 				// Guard against division by zero.
 				if (d == 0) {
@@ -126,7 +96,9 @@ namespace piranha
 				mpz_swap(mpq_denref(m_value.get_mpq_t()),mpq_denref(tmp.get_mpq_t()));
 			}
 			/// Constructor from double.
-			explicit mp_rational(const double &x):m_value(x) {}
+			explicit mp_rational(const double &x):ancestor(x) {}
+			ASSIGNMENT_OPERATOR(mp_rational,int)
+			ASSIGNMENT_OPERATOR(mp_rational,double)
 			/// Cast to double.
 			/**
 			 * Uses the get_d() GMP routine internally.
@@ -178,46 +150,6 @@ namespace piranha
 				}
 				return retval;
 			}
-			/// Equality operator.
-			bool operator==(const mp_rational &other) const
-			{
-				return (m_value == other.m_value);
-			}
-			/// Comparison operator.
-			bool operator<(const mp_rational &other) const
-			{
-				return (m_value < other.m_value);
-			}
-			/// In-place addition.
-			mp_rational &operator+=(const mp_rational &other)
-			{
-				m_value += other.m_value;
-				return *this;
-			}
-			/// In-place subtraction.
-			mp_rational &operator-=(const mp_rational &other)
-			{
-				m_value -= other.m_value;
-				return *this;
-			}
-			/// In-place multiplication.
-			mp_rational &operator*=(const mp_rational &other)
-			{
-				m_value *= other.m_value;
-				return *this;
-			}
-			/// In-place division.
-			/**
-			 * @throws zero_division_error if dividing by zero.
-			 */
-			mp_rational &operator/=(const mp_rational &other)
-			{
-				if (other.m_value == 0) {
-					piranha_throw(zero_division_error,"cannot divide by zero");
-				}
-				m_value /= other.m_value;
-				return *this;
-			}
 			/// Negate in-place.
 			/**
 			 * Implemented through the mpq_neg GMP function.
@@ -237,21 +169,6 @@ namespace piranha
 				retval.negate();
 				return retval;
 			}
-			// Operators against types directly compatible with mpq_class.
-			EQUALITY_OPERATOR(int)
-			EQUALITY_OPERATOR(double)
-			COMPARISON_OPERATOR(int)
-			COMPARISON_OPERATOR(double)
-			INPLACE_OPERATOR(mp_rational,=,double)
-			INPLACE_OPERATOR(mp_rational,=,int)
-			INPLACE_OPERATOR(mp_rational,+=,int)
-			INPLACE_OPERATOR(mp_rational,+=,double)
-			INPLACE_OPERATOR(mp_rational,-=,int)
-			INPLACE_OPERATOR(mp_rational,-=,double)
-			INPLACE_OPERATOR(mp_rational,*=,int)
-			INPLACE_OPERATOR(mp_rational,*=,double)
-			INPLACE_DIVISION(mp_rational,int)
-			INPLACE_DIVISION(mp_rational,double)
 			/// Exponentiation.
 			/**
 			 * @throws zero_division_error if power is negative and value is zero.
@@ -380,21 +297,7 @@ namespace piranha
 				}
 				return retval;
 			}
-		private:
-			mpq_class m_value;
 	};
-
-	#undef EQUALITY_OPERATOR
-	#undef COMPARISON_OPERATOR
-	#undef INPLACE_OPERATOR
-	#undef INPLACE_DIVISION
-
-	/// Overload out stream operator<< for piranha::mp_rational.
-	inline std::ostream &operator<<(std::ostream &o, const mp_rational &q)
-	{
-		o << q.m_value;
-		return o;
-	}
 
 	/// Overload in stream operator>> for piranha::mp_rational.
 	inline std::istream &operator>>(std::istream &i, mp_rational &q)
@@ -443,118 +346,51 @@ namespace std
 		return q.pow(y);
 	}
 
-	// Useful macros for operators.
-	#define INPLACE_REAL_OPERATOR(op,type) \
-	/** \brief In-place operator op for type. */ \
-	complex &operator op(const type &x) \
-	{ \
-		m_real op x; \
-		return *this; \
-	}
-	#define INPLACE_REAL_OPERATOR2(op,type) \
-	/** \brief In-place operator op for type. */ \
-	complex &operator op(const type &x) \
-	{ \
-		m_real op x; \
-		m_imag op x; \
-		return *this; \
-	}
-	#define INPLACE_REAL_ASSIGN(type) \
-	/** \brief Assign type. */ \
-	complex &operator=(const type &x) \
-	{ \
-		m_real = x; \
-		m_imag = 0; \
-		return *this; \
-	}
-	#define INPLACE_COMPLEX_OPERATOR(op,type) \
-	/** \brief In-place operator op for complex type. */ \
-	complex &operator op(const complex< type > &c) \
-	{ \
-		m_real op c.real(); \
-		m_imag op c.imag(); \
-		return *this; \
-	}
-	#define INPLACE_COMPLEX_MULT(type) \
-	/** \brief In-place operator *= for complex type. */ \
-	complex &operator*=(const complex< type > &c) \
-	{ \
-		return mult_by_complex(c); \
-	}
-	#define INPLACE_COMPLEX_DIV(type) \
-	/** \brief In-place operator /= for complex type. */ \
-	complex &operator/=(const complex< type > &c) \
-	{ \
-		return divide_by_complex(c); \
-	}
-	#define REAL_EQUALITY(type) \
-	/** \brief Equality operator for type. */ \
-	bool operator==(const type &x) const \
-	{ \
-		return (m_real == x && m_imag == 0); \
-	}
-	#define COMPLEX_EQUALITY(type) \
-	/** \brief Equality operator for complex type. */ \
-	bool operator==(const complex< type > &c) const \
-	{ \
-		return (m_real == c.real() && m_imag == c.imag()); \
-	}
-
 	/// Complex counterpart of piranha::mp_rational.
 	/**
 	 * Stores two piranha::mp_rational members as real and imaginary part.
 	 */
 	template <>
 	class complex<piranha::mp_rational>:
-		boost::field_operators<complex<piranha::mp_rational>,
-		boost::field_operators<complex<piranha::mp_rational>, int,
-		boost::field_operators<complex<piranha::mp_rational>, double,
-		boost::field_operators<complex<piranha::mp_rational>, piranha::mp_rational
-		> > > >
+		public piranha::complex_generic_mp_container<piranha::mp_rational,complex<piranha::mp_rational> >
 	{
-			friend ostream &operator<<(ostream &, const complex &);
+			typedef piranha::complex_generic_mp_container<piranha::mp_rational,complex<piranha::mp_rational> > ancestor;
 		public:
 			/// STL-like typedef for internal scalar type.
 			typedef piranha::mp_rational value_type;
 			/// Default constructor.
-			explicit complex(): m_real(),m_imag() {}
+			explicit complex(): ancestor() {}
 			/// Constructor from integer.
-			explicit complex(const int &n): m_real(n),m_imag() {}
-			/// Constructor from integer real and imaginary parts..
-			explicit complex(const int &r, const int &i): m_real(r),m_imag(i) {}
+			explicit complex(const int &n): ancestor(n) {}
+			/// Constructor from integer real and imaginary parts.
+			explicit complex(const int &r, const int &i): ancestor(r,i) {}
 			/// Constructor from complex integer.
-			explicit complex(const complex<int> &c): m_real(c.real()),m_imag(c.imag()) {}
+			explicit complex(const complex<int> &c): ancestor(c) {}
 			/// Constructor from double.
-			explicit complex(const double &x): m_real(x),m_imag() {}
+			explicit complex(const double &x): ancestor(x) {}
 			/// Constructor from double real and imaginary parts..
-			explicit complex(const double &r, const double &i): m_real(r),m_imag(i) {}
+			explicit complex(const double &r, const double &i): ancestor(r,i) {}
 			/// Constructor from complex double.
-			explicit complex(const complex<double> &c): m_real(c.real()),m_imag(c.imag()) {}
+			explicit complex(const complex<double> &c): ancestor(c) {}
 			/// Constructor from value_type.
-			explicit complex(const value_type &x): m_real(x),m_imag() {}
+			explicit complex(const value_type &x): ancestor(x) {}
 			/// Constructor from real and imaginary parts of type value_type.
-			explicit complex(const value_type &r, const value_type &i): m_real(r),m_imag(i) {}
+			explicit complex(const value_type &r, const value_type &i): ancestor(r,i) {}
 			/// Constructor from std::string.
 			/**
-			 * Will raise a value_error exception if string is not valid. A valid string is of the form
-			 * "(real,imag)", where "real" and "imag" are strings that can construct successfully a
-			 * piranha::mp_rational.
-			 * @see piranha::mp_rational::mp_rational(const std::string &).
-			 * @throws value_error if string is invalid.
-			 * @throws zero_division_error if string is valid but at least one denominator is zero.
+			 * @see complex_generic_mp_container::complex_generic_mp_container(const std::string &)
 			 */
-			explicit complex(const string &str): m_real(),m_imag()
-			{
-				construct_from_string(str.c_str());
-			}
+			explicit complex(const string &str): ancestor(str) {}
 			/// Constructor from C string.
 			/**
 			 * @see complex(const std::string &).
 			 */
-			explicit complex(const char *str): m_real(),m_imag()
-			{
-				construct_from_string(str);
-			}
+			explicit complex(const char *str): ancestor(str) {}
+			ASSIGNMENT_OPERATOR(complex,int)
+			ASSIGNMENT_OPERATOR(complex,double)
+			ASSIGNMENT_OPERATOR(complex,complex<int>)
+			ASSIGNMENT_OPERATOR(complex,complex<double>)
+			ASSIGNMENT_OPERATOR(complex,value_type)
 			/// Cast to complex double.
 			/**
 			 * @see piranha::mp_rational::operator double.
@@ -570,16 +406,6 @@ namespace std
 			operator complex<int>() const
 			{
 				return complex<int>((int)m_real,(int)m_imag);
-			}
-			/// Get const reference to the real part.
-			const value_type &real() const
-			{
-				return m_real;
-			}
-			/// Get const reference to the imaginary part.
-			const value_type &imag() const
-			{
-				return m_imag;
 			}
 			/// Swap content.
 			void swap(complex &other)
@@ -598,67 +424,6 @@ namespace std
 				boost::hash_combine(retval, hasher(m_imag));
 				return retval;
 			}
-			/// Equality operator.
-			bool operator==(const complex &other) const
-			{
-				return (m_real == other.m_real && m_imag == other.m_imag);
-			}
-			// Math operators.
-			/// In-place addition.
-			complex &operator+=(const complex &other)
-			{
-				m_real += other.m_real;
-				m_imag += other.m_imag;
-				return *this;
-			}
-			/// In-place subtraction.
-			complex &operator-=(const complex &other)
-			{
-				m_real -= other.m_real;
-				m_imag -= other.m_imag;
-				return *this;
-			}
-			/// In-place multiplication.
-			complex &operator*=(const complex &other)
-			{
-				return mult_by_complex(other);
-			}
-			/// In-place division.
-			complex &operator/=(const complex &other)
-			{
-				return divide_by_complex(other);
-			}
-			// Maths for other types.
-			REAL_EQUALITY(int)
-			REAL_EQUALITY(double)
-			REAL_EQUALITY(value_type)
-			COMPLEX_EQUALITY(int)
-			COMPLEX_EQUALITY(double)
-			INPLACE_REAL_ASSIGN(int)
-			INPLACE_REAL_ASSIGN(double)
-			INPLACE_REAL_ASSIGN(value_type)
-			INPLACE_COMPLEX_OPERATOR(=,int)
-			INPLACE_COMPLEX_OPERATOR(=,double)
-			INPLACE_REAL_OPERATOR(+=,int)
-			INPLACE_REAL_OPERATOR(+=,double)
-			INPLACE_REAL_OPERATOR(+=,value_type)
-			INPLACE_COMPLEX_OPERATOR(+=,int)
-			INPLACE_COMPLEX_OPERATOR(+=,double)
-			INPLACE_REAL_OPERATOR(-=,int)
-			INPLACE_REAL_OPERATOR(-=,double)
-			INPLACE_REAL_OPERATOR(-=,value_type)
-			INPLACE_COMPLEX_OPERATOR(-=,int)
-			INPLACE_COMPLEX_OPERATOR(-=,double)
-			INPLACE_REAL_OPERATOR2(*=,int)
-			INPLACE_REAL_OPERATOR2(*=,double)
-			INPLACE_REAL_OPERATOR2(*=,value_type)
-			INPLACE_REAL_OPERATOR2(/=,int)
-			INPLACE_REAL_OPERATOR2(/=,double)
-			INPLACE_REAL_OPERATOR2(/=,value_type)
-			INPLACE_COMPLEX_MULT(int)
-			INPLACE_COMPLEX_MULT(double)
-			INPLACE_COMPLEX_DIV(int)
-			INPLACE_COMPLEX_DIV(double)
 			/// Exponentiation.
 			/**
 			 * @throws zero_division_error if power is negative and value is zero.
@@ -681,79 +446,6 @@ namespace std
 				return pow(1. / double(n));
 			}
 		private:
-			// Square of absolute value.
-			value_type abs2() const
-			{
-				// NOTE: rewrite this in terms of multadd, when implemented.
-				value_type retval(m_real);
-				retval *= m_real;
-				retval += m_imag * m_imag;
-				return retval;
-			}
-			template <class Complex>
-			complex &divide_by_complex(const Complex &other)
-			{
-				// NOTE: rewrite this in terms of multadd, when implemented.
-				if (other.real() == 0 && other.imag() == 0) {
-					piranha_throw(zero_division_error,"cannot divide by zero");
-				}
-				// This is the divisor, i.e. the square of absolute value of other.
-				value_type div(other.real());
-				div *= other.real();
-				div += other.imag() * other.imag();
-				// The numerator looks like a multiplication with opposite signs.
-				const value_type tmp1 = m_imag * other.imag(), tmp2 = m_real * other.imag();
-				m_imag *= other.real();
-				m_imag -= tmp2;
-				m_real *= other.real();
-				m_real += tmp1;
-				// Now divide by divisor.
-				m_real /= div;
-				m_imag /= div;
-				return *this;
-			}
-			template <class Complex>
-			complex &mult_by_complex(const Complex &other)
-			{
-				// NOTE: rewrite this in terms of multadd, when implemented?
-				const value_type tmp1(m_imag * other.imag()), tmp2(m_real * other.imag());
-				// NOTE: we do imag first because if we modify real now, then it screws up the computation.
-				// m_imag is not used anymore as rhs from this point onwards.
-				m_imag *= other.real();
-				m_imag += tmp2;
-				m_real *= other.real();
-				m_real -= tmp1;
-				return *this;
-			}
-			void construct_from_string(const char *str)
-			{
-				string tmp(str);
-				// First let's trim the input string.
-				boost::trim(tmp);
-				// Next we split the input string into two parts, separated by the comma.
-				vector<string> split_v;
-				boost::split(split_v,tmp,boost::is_any_of(","));
-				// Now we check that the input string is well formed, i.e., it contains one comma,
-				// it starts and ends with round brackets and each element of the split vector contains at
-				// least two characters.
-				if (split_v.size() != 2 || split_v[0].size() < 2 || split_v[1].size() < 2 ||
-					split_v[0][0] != '(' || split_v[1][split_v[1].size() - 1] != ')') {
-					piranha_throw(value_error,"invalid string input");
-				}
-				// Finally, we try to build the two components from the split vector,
-				// discarding the positions in which there are the brackets.
-				m_real = value_type(string(&split_v[0][1],&split_v[0][split_v[0].size()]));
-				m_imag = value_type(string(&split_v[1][0],&split_v[1][split_v[1].size() - 1]));
-			}
-			complex invert() const
-			{
-				complex retval(*this);
-				retval.m_imag.negate();
-				const value_type div = abs2();
-				retval.m_real /= div;
-				retval.m_imag /= div;
-				return retval;
-			}
 			complex pow_int(const int &n) const {
 				complex retval;
 				// For negative powers, we must guard against division by zero.
@@ -805,17 +497,7 @@ namespace std
 				}
 				return retval;
 			}
-		private:
-			value_type	m_real;
-			value_type	m_imag;
 	};
-
-	#undef INPLACE_REAL_OPERATOR
-	#undef INPLACE_COMPLEX_OPERATOR
-	#undef INPLACE_COMPLEX_MULT
-	#undef INPLACE_COMPLEX_DIV
-	#undef REAL_EQUALITY
-	#undef COMPLEX_EQUALITY
 
 	/// Overload of standard swap function for std::complex<piranha::mp_rational>.
 	/**
@@ -844,13 +526,6 @@ namespace std
 		return qc.pow(y);
 	}
 
-	/// Overload out stream operator<< for std::complex<piranha::mp_rational>.
-	inline ostream &operator<<(ostream &o, const complex<piranha::mp_rational> &qc)
-	{
-		o << '(' << qc.m_real << ',' << qc.m_imag << ')';
-		return o;
-	}
-
 	/// Overload in stream operator>> for std::complex<piranha::mp_rational>.
 	inline std::istream &operator>>(std::istream &i, complex<piranha::mp_rational> &qc)
 	{
@@ -866,6 +541,8 @@ namespace std
 	{
 		return qc.hash();
 	}
+
+	#undef ASSIGNMENT_OPERATOR
 }
 
 #endif
