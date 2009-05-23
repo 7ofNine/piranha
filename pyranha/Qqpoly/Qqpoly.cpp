21a97e491999dcfe0085db40950e7ceb186aabd2
yardbird <yardbird@mojo.breestraat_3b>
2009-05-24 00:48:45 +0200

    Initial commit of qqpoly manipulator.

---
+++ pyranha/Qqpoly/Qqpoly.cpp
@@ -0,0 +1,49 @@
+/***************************************************************************
+ *   Copyright (C) 2007, 2008 by Francesco Biscani   *
+ *   bluescarni@gmail.com   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#include <boost/python/class.hpp>
+#include <boost/python/module.hpp>
+#include <string>
+#include <utility>
+
+#include "../../src/manipulators/qqpoly.h"
+#include "../series_instantiations.h"
+#include "../exceptions.h"
+
+using namespace boost::python;
+using namespace piranha;
+using namespace piranha::manipulators;
+using namespace pyranha;
+
+BOOST_PYTHON_MODULE(_Qqpoly)
+{
+	translate_exceptions();
+
+	std::pair<class_<qqpoly>, class_<qqpoly::term_type> > inst = series_basic_instantiation<qqpoly>(std::string("qqpoly"),
+			std::string("Multivariate polynomial with arbitrary-size rational coefficients."));
+	common_polynomial_instantiation(inst.first);
+	series_sub_instantiation<qqpoly, qqpoly>(inst.first);
+	std::pair<class_<qqpolyc>, class_<qqpolyc::term_type> > instc = series_basic_instantiation<qqpolyc>(std::string("qqpolyc"),
+			std::string("Multivariate polynomial with complex arbitrary-size rational coefficients."));
+	common_polynomial_instantiation(instc.first);
+	series_complex_instantiation(instc.first, inst.first);
+	series_sub_instantiation<qqpolyc, qqpoly>(instc.first);
+	series_sub_instantiation<qqpolyc, qqpolyc>(instc.first);
+}
