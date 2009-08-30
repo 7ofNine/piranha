ee4bf51ce0f8fe50bbe6e07e62518a5da1b3f6de
bluescarni <bluescarni@gmail.com>
2009-08-30 17:18:27 +0200

    Added function to detect if floating-point number is normal.

--- src/core/math.h
+++ src/core/math.h
@@ -438,10 +438,19 @@ namespace piranha
 	/**
 	 * Returns true.
 	 */
-	inline bool is_integer(const int &) {return true;}
+	inline bool is_integer(const int &)
+	{
+		return true;
+	}
 
 	/// Check if input argument can be represented exactly as an int type.
 	__PIRANHA_VISIBLE bool is_integer(const double &);
+
+	/// Check if floating-point number is normal.
+	inline bool fp_is_normal(const double &x)
+	{
+		return is_normal(x);
+	}
 }
 
 #endif
