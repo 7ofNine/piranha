4049845d3ecc0e8fd303ef8cd09bae603efd0662
bluescarni <bluescarni@gmail.com>
2009-09-21 22:53:47 +0200

    Initial import of boost libraries 1.40.0.

--- pyranha/Celmec/constant_thrust.py
+++ pyranha/Celmec/constant_thrust.py
@@ -123,6 +123,79 @@ def build_analytical(s0):
 		return retval
 	ct.t_eval = t_eval
 
+def build_poincare(s0):
+	from copy import copy
+	from pyranha import truncators
+	from pyranha.Celmec import oe2mdelaunay, mdelaunay2poincare, lieS
+	from pyranha.Math import integrate, partial
+	from pyranha.Core import psym, rational
+	from pyranha.Qqps import qqps, qqpsc
+	import pyranha.Celmec.constant_thrust as ct
+	# Some parameters.
+	# NOTE: these could depend on a parametric numerical precision.
+	pert_order = 1
+	H1_exp_order = 4 # Order of ecc + incl in the initial expansion of the perturbing Hamiltonian.
+	md_mom = ['Lam','P','Q']
+	md_coord = ['lam','p','q']
+	# Create needed series.
+	Lam, P, Q, lam, p, q, lam0, eps = [qqps(psym(n)) for n in md_mom + md_coord + ['lam0','eps']]
+	# Set truncators for initial expansion of perturbing Hamiltonian.
+	truncators.unset()
+	truncators.degree.set(['P','Q'],rational(H1_exp_order + 1,2))
+	# Hamiltonian.
+	H = -1 * (2 * Lam ** 2) ** -1 + eps * H1([Lam,P,Q,lam,p,q])
+	truncators.unset()
+	# Transform int Poincare' variables.
+	p_mom = ['Lam','y','z']
+	p_coord = ['lam','x','v']
+	Lam, y, z, lam, x, v, two = [qqps(psym(n)) for n in p_mom + p_coord + ['two']]
+	H = H.ei_sub('p',qqpsc(y * (two * P) ** rational(-1,2),x * (two * P) ** rational(-1,2))).ei_sub('q',qqpsc(z * (two * Q) ** rational(-1,2),v * (two * Q) ** rational(-1,2))).sub('two',qqps(2)).sub('P',(x ** 2 + y ** 2) / 2).sub('Q',(z ** 2 + v ** 2) / 2)
+	# Solver for the homological equation.
+	def he_s(Hk):
+		# This will integrate wrt lambda the part of the k-th order of the Hamiltonian that depends on the coordinates
+		# and return the result as the generator for the Lie transform.
+		return integrate(Lam ** 3 * Hk.filtered([lambda t: any(n in t[0] or n in t[1] for n in p_coord)]),'lam')
+		#return integrate(Lam ** 3 * Hk.filtered(lambda t: t[1].h_order('lam') != 0),'lam')
+	# Truncator for Lie Series expansions.
+	truncators.degree.set(['eps'],pert_order + 1)
+	# Clear return values.
+	# Replace lambda with delta_lam + lambda_0.
+	ct.H_list = [H.sub('lam',lam + lam0)]
+	ct.chi_list = []
+	ct.direct = [[Lam, y, z, lam, x, v]]
+	ct.inverse = [[Lam, y, z, lam, x, v]]
+	ct.freqs = []
+	# Start the iteration.
+	for i in range(0,pert_order):
+		cur_H = ct.H_list[-1]
+		print('\033[1;32mCurrent perturbative order: %d\033[1;m' % (i + 1))
+		Hk = cur_H.filtered([None,lambda t: t[1].degree('eps') == i + 1]) * eps ** (-i -1) 
+		print('Solving homological equation...')
+		chi = he_s(Hk)
+		print('Calculating Hamiltonian...')
+		ct.H_list.append(lieS(eps ** (i + 1),chi,cur_H,p_mom,p_coord))
+		ct.chi_list.append(chi)
+		print('Calculating ' +'\033[1;31mdirect\033[1;m ' +'coordinate transformation...')
+		ct.direct.append([lieS(eps ** (i + 1),chi,arg,p_mom,p_coord) for arg in ct.direct[-1]])
+		print('Calculating ' +'\033[1;31minverse\033[1;m ' +'coordinate transformation...')
+		ct.inverse.append([lieS(eps ** (i + 1),-chi,arg,p_mom,p_coord) for arg in ct.inverse[-1]])
+	# Build dictionaries for evaluation.
+	var_names = p_mom + p_coord + ['eps','two','lam0']
+	eval_direct = dict(zip(var_names,mdelaunay2poincare(oe2mdelaunay(s2oe(s0))) + [ct.thrust,2.,0]))
+	# We have to take into account the modification of lambda for delta_lam.
+	eval_direct['lam0'] = eval_direct['lam']
+	eval_direct['lam'] = 0
+	# Now te initial conditions for the inverse.
+	eval_inverse = dict(zip(var_names,[ct.inverse[-1][i].eval(eval_direct) for i in range(0,6)] + [ct.thrust,2.,eval_direct['lam0']]))
+	# Now the frequencies of the angles.
+	ct.freqs = dict(zip(p_coord,[partial(ct.H_list[-1],arg).eval(eval_inverse) for arg in p_mom]))
+	def t_eval(t):
+		retval = copy(eval_inverse)
+		for n in p_coord:
+			retval[n] += freqs[n] * t
+		return retval
+	ct.t_eval = t_eval
+
 def s2oe(s):
 	from numpy.linalg import norm
 	from numpy import array, cross, dot
